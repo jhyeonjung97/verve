5b7e4b80667dd2c1d4dd9c74c9808fbed63383fe
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-02-29 22:40:39 -0800

    .

--- bader.py
+++ bader.py
@@ -30,40 +30,36 @@ def get_bader_charges(traj):
     file.close()
     for j in [1, 0, -4, -3, -2, -1]:
         del lines[j]
-
-	newlines = []
-	for line in lines:
-		newline = map(float,line.split())
-		newlines.append(list(newline))
-
-	newlines = np.array(newlines)
-	#print ((newlines[:,4]))
-	charge = newlines[:,4]
-
-	#atoms=io.read('OUTCAR')
-	atoms=read(traj)
-	write('qn.xyz',atoms)
-
-	# get xyz file  
-	filelist = glob.glob('*.xyz')
-	xyzfile = filelist[0]		
-	file = open(xyzfile,"r")
-	lines = file.readlines() # This is what you forgot
-	file.close()
-	for j in [1, 0]:
-		del lines[j]
-	
-
-	del newlines
-	newlines = []
-	for line in lines:
-		newline =line.split() # map(float,line.split())
-		newlines.append(newline)
-
-	newarray = np.array(newlines)
-	name = newarray[:,0]
-
-	# Define the dictionary for reference charges of each element
+        
+    newlines = []
+    for line in lines:
+        newline = map(float,line.split())
+        newlines.append(list(newline))
+        
+    newlines = np.array(newlines)
+    charge = newlines[:,4]
+    
+    atoms=read(traj)
+    write('qn.xyz',atoms)
+    
+    filelist = glob.glob('*.xyz')
+    xyzfile = filelist[0]
+    file = open(xyzfile,"r")
+    lines = file.readlines()
+    file.close()
+    for j in [1, 0]:
+        del lines[j]
+        
+    del newlines
+    newlines = []
+    for line in lines:
+        newline =line.split()
+        newlines.append(newline)
+        
+    newarray = np.array(newlines)
+    name = newarray[:,0]
+    
+    # Define the dictionary for reference charges of each element
     chargedict = {
         'Pt': 10, 'Ce': 12, 'W': 6, 'Sm': 11, 'Ti': 4, 'V': 5, 'Cr': 6, 'Mn': 7,
         'Fe': 8, 'Co': 9, 'Ni': 10, 'Cu': 11, 'Zn': 12, 'Ga': 3, 'Ge': 4, 'As': 5,
