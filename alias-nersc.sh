7fb1f67c5d0e458d43038564b5e4a47467bd12b3
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-02-29 18:06:10 -0800

    .

--- alias-nersc.sh
+++ alias-nersc.sh
@@ -7,7 +7,6 @@ alias vasp5='mv /global/homes/j/jiuy97/bin/vasp_outcar_parsers5.py /global/homes
 alias vasp6='mv /global/homes/j/jiuy97/bin/vasp_outcar_parsers6.py /global/homes/j/jiuy97/.local/lib/python3.11/site-packages/ase/io/vasp_parsers/vasp_outcar_parsers.py'
 alias mystat='squeue -o "%.18i %.9P %.18j %.8u %.10T %.8M %.10l %.6D %R" --me --sort=i'
 
-alias sub='sbatch submit.sh'
 alias qdel='scancel'
 
 alias time='grep sec OUTCAR'
