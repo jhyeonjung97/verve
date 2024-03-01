940a7161db1212512b74ca360b46a5fdb5a000ba
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-02-29 17:18:20 -0800

    .

--- incar.py
+++ incar.py
@@ -1,8 +1,17 @@
 import argparse
 
+# Your existing import statements and code here...
+
+def modify_parameters(tag_to_modify, new_value):
+    if tag_to_modify in locals():
+        locals()[tag_to_modify] = new_value
+    else:
+        with open('opt_bulk.py', 'a') as f:
+            f.write(f"{tag_to_modify} = {new_value}\n")
+
 def main():
-    parser = argparse.ArgumentParser(description='Modify VASP calculation parameters in opt_bulk.py')
-    parser.add_argument('tag', type=str, help='Parameter tag to modify')
+    parser = argparse.ArgumentParser(description='Modify or add VASP calculation parameters in opt_bulk.py')
+    parser.add_argument('tag', type=str, help='Parameter tag to modify or add')
     parser.add_argument('value', type=str, help='Value to set for the parameter')
 
     args = parser.parse_args()
@@ -10,11 +19,7 @@ def main():
     tag_to_modify = args.tag
     new_value = args.value
 
-    if tag_to_modify not in locals():
-        print(f"Error: Tag '{tag_to_modify}' not found in opt_bulk.py.")
-        return
-
-    locals()[tag_to_modify] = new_value
+    modify_parameters(tag_to_modify, new_value)
 
     # Your existing code to create 'calc' instance...
 
