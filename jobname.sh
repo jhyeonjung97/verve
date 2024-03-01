38fab23afde6b2d449c3546d3fba628bf3342a89
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-02-29 21:12:50 -0800

    .

--- jobname.sh
+++ jobname.sh
@@ -9,20 +9,14 @@ if [[ $1 == '-h' ]] || [[ -z $1 ]]; then
     usage_error
 fi
 
-function numb {
-    [[ $1 =~ ^[0-9]+$ ]] || usage_error
-}
-
 name=$2
-
-if [[ -z $2 ]]; then
+if [[ -z $name ]]; then
     sed -i "/#SBATCH -J/c\#SBATCH -J $name" submit.sh
     if [[ -f lobster.sh ]]; then
         sed -i "/#SBATCH -J/c\#SBATCH -J $name" lobster.sh
     fi
     exit 0
 elif [[ $1 == '-r' ]]; then
-    numb $3
     if [[ -z $4 ]]; then
         SET=$(seq 1 $3)
     elif [[ -z $5 ]]; then
@@ -31,7 +25,6 @@ elif [[ $1 == '-r' ]]; then
         usage_error
     fi
 else
-    numb $3
     name=$1
     SET=$(seq 1 $2)
 fi
