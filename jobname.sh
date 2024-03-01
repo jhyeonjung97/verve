43dad0d12b8d9c1bdcef6b61d90d5ba649033295
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-02-29 18:02:55 -0800

    .

--- jobname.sh
+++ jobname.sh
@@ -1,41 +1,46 @@
 #!/bin/bash
 
 function usage_error {
-    echo 'Usage: name (-r) [jobname] [dir#1] [dir#2]'
+    echo 'Usage: name_slurm.sh (-r) [jobname] [dir#1] [dir#2]'
     exit 1
 }
 
-if [[ $1 == '-h' || $1 == '--help' || -z $1 ]]; then
+if [[ $1 == '-h' ]] || [[ -z $1 ]]; then
     usage_error
 fi
 
-if [[ -z $2 ]]; then
-    job_name=$1
-    sed -i "/#SBATCH -J/c\#SBATCH -J $job_name" submit.sh
+function numb {
+    [[ $1 =~ ^[0-9]+$ ]] || usage_error
+}
+
+name=$2
+
+if [[ -z $3 ]]; then
+    sed -i "/#SBATCH -J/c\#SBATCH -J $name" run_slurm.sh
     if [[ -f lobster.sh ]]; then
-        sed -i "/#SBATCH -J/c\#SBATCH -J $job_name" lobster.sh
+        sed -i "/#SBATCH -J/c\#SBATCH -J $name" lobster.sh
     fi
     exit 0
-else
-    if [[ $1 == '-r' && -z $2 ]]; then
-        usage_error
-    elif [[ $1 == '-r' ]]; then
-        job_name=$2
-        DIR='*/'
-    elif [[ $1 == '-s' && -n $2 ]]; then
-        DIR=$@
+elif [[ $1 == '-r' ]]; then
+    numb $3
+    if [[ -z $4 ]]; then
+        SET=$(seq 1 $3)
+    elif [[ -z $5 ]]; then
+        SET=$(seq $3 $4)
     else
-        DIR=$(seq ${1:-1} ${2:-$1})
+        usage_error
     fi
+else
+    numb $3
+    name=$1
+    SET=$(seq 1 $2)
 fi
 
-# Loop through directories and update job names
-for i in $DIR
+# loop
+for i in $SET
 do
     i=${i%/}
     j=$(echo $i | cut -c 1)
-    sed -i "/#SBATCH -J/c\#SBATCH -J $job_name$j" $i*/submit.sh
+    sed -i "/#SBATCH -J/c\#SBATCH -J $name$j" $i/run_slurm.sh
 done
-
-# Display updated job names
-grep '#SBATCH -J' */submit.sh
+grep '#SBATCH -J' */run_slurm.sh
