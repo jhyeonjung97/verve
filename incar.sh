e34b6c7156720cb2acf996c0132e34b1dc25bff0
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-02-29 17:26:22 -0800

    .

--- incar.sh
+++ incar.sh
@@ -22,9 +22,8 @@ fi
 # Update the file with the specified pattern=value
 
 if [[ -n $(grep $pattern $filename) ]]; then
-    sed -i "/calc = vasp_calculator.Vasp(/a\    $pattern=" "$filename"
+    sed -i "/calc = vasp_calculator.Vasp(/a\                            $pattern=" "$filename"
 fi
 
 sed -i "s/$pattern=.*/$pattern=$value/" $filename
-sed -i "/calc = vasp_calculator.Vasp(/a\    $text_to_add" "$file"
 echo "Pattern '$pattern' updated to '$pattern=$value' in '$filename'."
