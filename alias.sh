f4c1c95b4912c2209f3ab901472cd26fe7d7aa85
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-02-29 22:48:15 -0800

    .

--- alias.sh
+++ alias.sh
@@ -34,8 +34,8 @@ alias ee='grep E0 stdout* | tail -n 3'
 alias freq='grep THz OUTCAR'
 alias fermi='grep E-fermi OUTCAR | tail -n 1'
 alias magnet='awk "/magnetization \(x\)/,/tot /" OUTCAR'
-alias bader='sh ~/bin/verve/bader.py'
-alias dos3='sh ~/bin/shoulder/dos3.sh'
+alias bader='python ~/bin/verve/bader.py'
+alias dos3='python ~/bin/shoulder/dos3.py'
 
 # Git
 alias orange='dir_now=$PWD
