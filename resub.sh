9e3dcd8355ed068e2679e5280014ef81774f533a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-02-29 21:06:02 -0800

    .

---
+++ resub.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function usage_error {
+    echo 'Usage: resub.sh [-r | -s dir]'
+    exit 1
+}
+
+if [[ $1 == '-h' || $1 == '--help' || -z $1 ]]; then
+    usage_error
+fi
+
+if [[ -z $1 ]]; then
+    rm STD*
+    sbatch submit.sh
+    exit 0
+else
+    if [[ $1 == '-r' ]]; then
+        DIR='*/'
+    elif [[ $1 == '-s' && -n $2 ]]; then
+        DIR=$2
+    else
+        DIR=$(seq ${1:-1} ${2:-$1})
+    fi
+fi
+
+for i in $DIR
+do
+    i=${i%/}
+    cd $i* && rm STD* && sbatch submit.sh && cd ..
+done
