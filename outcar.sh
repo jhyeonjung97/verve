7ea83e9e8b878445c97bcf0477fda690e29e69cd
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-05 11:30:18 -0800

    .

---
+++ outcar.sh
@@ -0,0 +1,39 @@
+mag_tag=0
+chg_tag=0
+ene_tag=0
+while getopts ":mce" opt; do
+  case $opt in
+    m)
+      mag_tag=1
+      ;;
+    c)
+      chg_tag=1
+      ;;
+    e)
+      ene_tag=1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [[ $mag_tag == 1 ]]; then
+    pattern_s='magnetization \(x\)'
+    pattern_e='tot '
+elif [[ $chg_tag == 1 ]]; then
+    pattern_s='total charge '
+    pattern_e='tot '
+elif [[ $ene_tag == 1 ]]; then
+    pattern_s='Free energy of the ion-electron system \(eV\)'
+    pattern_e='free energy '
+fi
+
+n=$(awk "/$pattern_s/{flag=1;next}/$pattern_e/{if(flag){count++;flag=0}}END{print count}" OUTCAR)
+m=$(awk "/$pattern_s/{count=0;flag=1;next}/$pattern_e/{if(flag){print count;flag=0}}flag{count++}" OUTCAR | tail -n 1)
+awk "/$pattern_s/,/$pattern_e/" OUTCAR | tail -n $(($m+1))
