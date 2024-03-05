5fd6ffcfd6724ea2310d5811aa76b25ff16de06f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-04 21:45:29 -0800

    .

--- cell-size.py
+++ cell-size.py
@@ -6,7 +6,7 @@ def main():
     parser = argparse.ArgumentParser(description='Command-line options example')
 
     parser.add_argument('-r', '--ratio', action='store_true', default=False, help='use ratio for scaling')
-    parser.add_argument('factor', type=float, default=1.0, help='scaling factor or fixed size')
+    parser.add_argument('-f', '--factor', type=float, default=1.0, help='scaling factor or fixed size')
     parser.add_argument('-o', '--output', type=str, default=None, help='the name of output file')
 
     args = parser.parse_args()
