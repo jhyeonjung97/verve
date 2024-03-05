b5819a383930ac4a5b3f0334c8edf8d6bfa4d288
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-05 10:55:34 -0800

    .

---
+++ magnet.sh
@@ -0,0 +1,3 @@
+n = $(awk '/magnetization \(x\)/{flag=1;next}/tot /{if(flag){count++;flag=0}}END{print count}' OUTCAR)
+m = $(awk '/magnetization \(x\)/{count=0;flag=1;next}/tot /{if(flag){print count;flag=0}}flag{count++}' OUTCAR | tail -n 1)
+awk "/magnetization \(x\)/,/tot /" OUTCAR | tail -n $m
