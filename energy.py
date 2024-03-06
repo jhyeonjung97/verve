06204740a4e843d94c58e0cb76a26b9bdb59cee7
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-05 16:28:34 -0800

    .

--- energy.py
+++ energy.py
@@ -112,7 +112,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--ref-type', type=str, default=None, help='Adjust values by subtracting the minimum')
     parser.add_argument('-d', '--dir-range', type=str, default=None, help='Range of directories to investigate, e.g., "3,6"')
-    parser.add_argument('-p', '--patterns', nargs='+', default='TOTEN', help='Patterns to search and plot')
+    parser.add_argument('-p', '--patterns', nargs='+', default=['TOTEN'], help='Patterns to search and plot')
     parser.add_argument('-a', '--all', action='store_true', default=False, help='Show all components')
     parser.add_argument('--no-total', action='store_true', default=False, help='No show total energy')
     parser.add_argument('--xlabel', default='Lattice parameter (Å)', type=str, help="x-axis title of the figure")
