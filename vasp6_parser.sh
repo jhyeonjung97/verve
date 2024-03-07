e295bf51b86b12b9d2bc271f403a66c166c2cece
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-07 11:48:55 -0800

    .

--- vasp6_parser.sh
+++ vasp6_parser.sh
@@ -1,1 +1,1 @@
-cp ~/bin/shoulder/vasp_outcar_parser.py /global/homes/j/jiuy97/.local/perlmutter/python-3.11/lib/python3.11/site-packages/ase/io/vasp_parsers
+cp ~/bin/shoulder/vasp_outcar_parsers.py /global/homes/j/jiuy97/.local/perlmutter/python-3.11/lib/python3.11/site-packages/ase/io/vasp_parsers
