91f85ba50d30dad8fd3fec723cde492c4884144c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-06 18:39:05 -0800

    .

--- alias.sh
+++ alias.sh
@@ -50,7 +50,7 @@ alias cobi='python ~/bin/playground/aloha/cobi.py'
 #SSH
 alias hi='~/bin/verve/sshproxy.sh -u jiuy97
 ssh -l jiuy97 -i ~/.ssh/nersc perlmutter.nersc.gov'
-alias bye='sbash; orange; shoulder; verve'
+alias bye='sbash; orange; shoulder; verve; rsync'
 alias burning='ssh -X -Y hyeonjung@burning.postech.ac.kr -p 54329'
 alias snu='ssh -X -Y hyeonjung@210.117.209.87'
 alias x2658='ssh -X -Y x2658a09@nurion.ksc.re.kr'
--- archieve.sh
+++ archieve.sh
@@ -1,5 +1,18 @@
 dir_now=$PWD
-cd /Users/hailey/jiuy97@stanford.edu\ -\ Google\ Drive/My\ Drive
+
+if ${here} == 'slac'
+    usr='jiuy97'
+elif ${here} == 'mac'; then
+    usr='hailey'
+fi
+
+cd /Users/$usr/jiuy97@stanford.edu\ -\ Google\ Drive/My\ Drive
+
+cp -r /Users/jiuy97/bin/orange/* ./bin/orange
+cp -r /Users/jiuy97/bin/shoulder/* ./bin/shoulder
+cp -r /Users/jiuy97/bin/verve/* ./bin/verve
+
 /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/1_cation .
 /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/2_icohp .
+
 cd $dir_now
