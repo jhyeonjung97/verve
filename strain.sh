16ae9f8e3701726cb755150afda456c61a5677a8
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-07 17:47:42 -0800

    .

---
+++ strain.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cp relaxed/restart.json .
+mkdir 1_-0.3  2_-0.2  3_-0.1  4_0.0  5_+0.1  6_+0.2  7_+0.3
+
+python ~/bin/verve/cell-size.py -f -0.3 -o start1.traj
+python ~/bin/verve/cell-size.py -f -0.2 -o start2.traj
+python ~/bin/verve/cell-size.py -f -0.1 -o start3.traj
+python ~/bin/verve/cell-size.py -f 0.0 -o start4.traj
+python ~/bin/verve/cell-size.py -f 0.1 -o start5.traj
+python ~/bin/verve/cell-size.py -f 0.2 -o start6.traj
+python ~/bin/verve/cell-size.py -f 0.3 -o start7.traj
+
+for i in {1..7}
+do
+    mv "start$i.traj" $i*/start.traj
+done
+
+rm 'restart.json'
+ls */
