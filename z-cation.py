d7790a51ac6a9ac1af410cf9beaa55e531aec1a6
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-08 15:44:54 -0800

    .

--- z-cation.py
+++ z-cation.py
@@ -4,18 +4,14 @@ from sys import argv
 from ase.io import read, write
 
 file = argv[1]
-print('file: ', file) #print
 atoms = read(file)
 name, ext = os.path.splitext(file)
-print('file: ', name, ext) #print
 
-print('len(argv): ', len(argv)) #print
 if len(argv) < 3:
     print("No oxygen atom indices provided.")
     exit(1)
 
 oxygen_indices = list(map(int, argv[2:]))
-print('oxygen_indices: ', oxygen_indices) #print
 for index in oxygen_indices:
     if atoms[index].symbol != 'O':
         print(f"This atom is not oxygen: {index}")
@@ -25,30 +21,44 @@ for i in range(len(oxygen_indices) - 1):
     oxygen1 = oxygen_indices[i]
     oxygen2 = oxygen_indices[i+1]
     hydrogen11 = None
+    hydrogen12 = None
     hydrogen21 = None
     hydrogen22 = None
-    print(oxygen1, oxygen2) #print
 
     for atom in atoms:
         if atom.symbol == 'H':
             d1 = np.linalg.norm(atoms[oxygen1].position - atom.position)
             d2 = np.linalg.norm(atoms[oxygen2].position - atom.position)
             if 0.5 < d1 < 1.5:
-                if hydrogen11 is None \
-                or d1 < np.linalg.norm(atoms[oxygen1].position - atoms[hydrogen11].position):
+                if hydrogen11 is None:
                     hydrogen11 = atom.index
+                elif hydrogen12 is None:
+                    hydrogen12 = atom.index
+                else:
+                    print(f"Too many hydrogens {atom} were found near oxygen1: {oxygen1}")
             if 0.5 < d2 < 1.5:
                 if hydrogen21 is None:
                     hydrogen21 = atom.index
                 elif hydrogen22 is None:
                     hydrogen22 = atom.index
-    print(hydrogen11, hydrogen21, hydrogen22) #print
+                elif atom.index == hydrogen:
+                    continue
+                else:
+                    print(f"Too many hydrogens {atom} were found near oxygen2: {oxygen2}")
+    
+    d1 = np.linalg.norm(atoms[hydrogen11].position - atoms[oxygen2].position)
+    d2 = np.linalg.norm(atoms[hydrogen12].position - atoms[oxygen2].position)
+    if d1 < d2:
+        hydrogen = hydrogen11
+    else:
+        hydrogen = hydrogen12
+    print('hydrogen:', hydrogen) #print
 
-    if not hydrogen11:
-        print(f"Not all required hydrogens were found near the oxygen atom: {oxygen1}")
+    if hydrogen11 is None or hydrogen12 is None:
+        print(f"Not all required hydrogens were found near the oxygen1: {oxygen1}")
         exit(1)
-    if not hydrogen21 or not hydrogen22:
-        print(f"Not all required hydrogens were found near the oxygen atom: {oxygen2}")
+    if hydrogen21 is None or hydrogen22 is None:
+        print(f"Not all required hydrogens were found near the oxygen2: {oxygen2}")
         exit(1)
                       
     O = atoms[oxygen2].position
@@ -61,16 +71,19 @@ for i in range(len(oxygen_indices) - 1):
     B2 = 2 * M - B1
     C1 = 2 * O - B1
     C2 = 2 * O - B2
-    print(O, M,
-          A1, A2,
-          B1, B2,
-          C1, C2) #print
+    print('O: ', O)
+    print('M: ', M)
+    print('A1: ', A1)
+    print('A2: ', A2)
+    print('B1: ', B1)
+    print('B2: ', B2)
+    print('C1: ', C1)
+    print('C2: ', C2) #print
 
-    d1 = np.linalg.norm(atoms[hydrogen11].position - B1)
-    d2 = np.linalg.norm(atoms[hydrogen11].position - B2)
+    d1 = np.linalg.norm(atoms[hydrogen].position - B1)
+    d2 = np.linalg.norm(atoms[hydrogen].position - B2)
     if d1 < d2:
-        atoms[hydrogen11].position = B1
+        atoms[hydrogen].position = B1
     else:
-        atoms[hydrogen11].position = B2
-    print(hydrogen11, atoms[hydrogen11.position]) #print
+        atoms[hydrogen].position = B2
     write(f'{name}_{i}{ext}', atoms)
