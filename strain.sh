3446e5946a923bb7c9b82e3013a959b190ae2fe6
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-09 21:31:29 -0800

    .

--- strain.sh
+++ strain.sh
@@ -2,46 +2,42 @@
 
 if [[ -z $1 ]]; then
     pwd
-    read -p 'which structure file?: ' file
+    read -p 'Enter the name of the structure file: ' file
 else
     file=$1
 fi
 
-if [[ $file == '*CONTCAR']] || [[ $file == '*.traj' ]]; then
+if [[ $file == *CONTCAR ]] || [[ $file == *.traj ]]; then
     name='start'
     ext='.traj'
-    ase convert $file start.traj
-    python ~/bin/verve/cell-size.py -f -0.3 -o start1.traj
-    python ~/bin/verve/cell-size.py -f -0.2 -o start2.traj
-    python ~/bin/verve/cell-size.py -f -0.1 -o start3.traj
-    python ~/bin/verve/cell-size.py -f 0.0 -o start4.traj
-    python ~/bin/verve/cell-size.py -f 0.1 -o start5.traj
-    python ~/bin/verve/cell-size.py -f 0.2 -o start6.traj
-    python ~/bin/verve/cell-size.py -f 0.3 -o start7.traj
-elif [[ $file == '*.json' ]]; then
+    ase convert "$file" start.traj
+elif [[ $file == *.json ]]; then
     name='restart'
     ext='.json'
-    ase convert $file restart.json
-    python ~/bin/verve/cell-size.py -f -0.3 -o restart1.json
-    python ~/bin/verve/cell-size.py -f -0.2 -o restart2.json
-    python ~/bin/verve/cell-size.py -f -0.1 -o restart3.json
-    python ~/bin/verve/cell-size.py -f 0.0 -o restart4.json
-    python ~/bin/verve/cell-size.py -f 0.1 -o restart5.json
-    python ~/bin/verve/cell-size.py -f 0.2 -o restart6.json
-    python ~/bin/verve/cell-size.py -f 0.3 -o restart7.json
+    ase convert "$file" restart.json
+else
+    echo "Unsupported file type. Please provide a CONTCAR, .traj, or .json file."
+    exit 1
 fi
+
+python ~/bin/verve/cell-size.py -f -0.3 -o "${name}1$ext"
+python ~/bin/verve/cell-size.py -f -0.2 -o "${name}2$ext"
+python ~/bin/verve/cell-size.py -f -0.1 -o "${name}3$ext"
+python ~/bin/verve/cell-size.py -f 0.0 -o "${name}4$ext"
+python ~/bin/verve/cell-size.py -f 0.1 -o "${name}5$ext"
+python ~/bin/verve/cell-size.py -f 0.2 -o "${name}6$ext"
+python ~/bin/verve/cell-size.py -f 0.3 -o "${name}7$ext"
 
 mkdir 1_-0.3  2_-0.2  3_-0.1  4_0.0  5_+0.1  6_+0.2  7_+0.3
 
-for dir in */
-do
-    i=$(echo ${dir%/} | cut -c 1)
-    if [[ -s $name$i$ext ]]; then
-        mv $name$i$ext $dir$name$ext
-        echo "mv $name$i$ext $dir$name$ext"
-        cp ~/bin/shoulder/lobsterin $dir
+for dir in */; do
+    i=$(echo "${dir%/}" | cut -c 1)
+    if [[ -s "$name$i$ext" ]]; then
+        mv "$name$i$ext" "$dir${name}$ext"
+        cp ~/bin/shoulder/lobsterin "$dir"
+        echo "Moved $name$i$ext to $dir${name}$ext"
     fi
 done
 
-~/bin/rm_mv $name$ext original.traj
-echo "\033[0;31mPrepare submit script and spread into the directories\033[0m"
+~/bin/rm_mv "${name}$ext" original.traj
+echo -e "\033[0;31mPrepare submit script and spread into the directories\033[0m"
