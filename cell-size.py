698d40fc56ff9a1e0d9a5de6208c024751e6d5dc
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-09 21:41:58 -0800

    .

--- cell-size.py
+++ cell-size.py
@@ -5,22 +5,31 @@ from os import path
 def main():
     parser = argparse.ArgumentParser(description='Command-line options example')
 
+    parser.add_argument('-i', '--input', type=str, required=True, help='input file name (json or traj)')
     parser.add_argument('-r', '--ratio', action='store_true', default=False, help='use ratio for scaling')
     parser.add_argument('-f', '--factor', type=float, default=1.0, help='scaling factor or fixed size')
     parser.add_argument('-o', '--output', type=str, default=None, help='the name of output file')
 
     args = parser.parse_args()
 
-    if path.exists('restart.json'):
-        atoms = read('restart.json')
-        write('original.traj', atoms)
-    elif path.exists('CONTCAR'):
-        atoms = read('CONTCAR')
-        write('original.traj', atoms)
-    elif path.exists('start.traj'):
-        atoms = read('start.traj')
-        write('original.traj', atoms)
-        
+    input_file = args.input
+
+    if not path.exists(input_file):
+        print(f"Error: File '{input_file}' not found.")
+        return
+
+    file_ext = path.splitext(input_file)[1]
+
+    if file_ext == '.json' or file_ext == '.traj':
+        atoms = read(input_file)
+    else:
+        print("Error: Unsupported file format. Please provide a JSON or Trajectory file.")
+        return
+
+    magmoms = None
+    if 'magmom' in atoms.arrays:
+        magmoms = atoms.get_initial_magnetic_moments().copy()
+
     factor = args.factor
     if not args.ratio:
         factor = (atoms.cell[0][0] + factor) / atoms.cell[0][0]
@@ -28,10 +37,13 @@ def main():
     atoms.set_cell(atoms.get_cell() * factor, scale_atoms=True)
 
     # Overwrite the original file with the modified atoms object
-    if args.output is None:
-        write('start.traj', atoms)
+    if args.output:
+        output_file = args.output
+    elif: file_ext == '.json' 
+        output_file = 'restart.json'
     else:
-        write(args.output, atoms)
+        output_file = 'start.traj'
+    write(output_file, atoms)
 
 if __name__ == "__main__":
     main()
--- strain.sh
+++ strain.sh
@@ -2,12 +2,12 @@
 
 if [[ -z $1 ]]; then
     pwd
-    read -p 'Enter the name of the structure file: ' file
+    read -p 'Enter the name of the structure file (traj or json): ' file
 else
     file=$1
 fi
 
-if [[ $file == *CONTCAR ]] || [[ $file == *.traj ]]; then
+if [[ $file == *.traj ]]; then
     name='start'
     ext='.traj'
     ase convert "$file" start.traj
@@ -20,13 +20,13 @@ else
     exit 1
 fi
 
-python ~/bin/verve/cell-size.py -f -0.3 -o "${name}1$ext"
-python ~/bin/verve/cell-size.py -f -0.2 -o "${name}2$ext"
-python ~/bin/verve/cell-size.py -f -0.1 -o "${name}3$ext"
-python ~/bin/verve/cell-size.py -f 0.0 -o "${name}4$ext"
-python ~/bin/verve/cell-size.py -f 0.1 -o "${name}5$ext"
-python ~/bin/verve/cell-size.py -f 0.2 -o "${name}6$ext"
-python ~/bin/verve/cell-size.py -f 0.3 -o "${name}7$ext"
+python ~/bin/verve/cell-size.py -i $file -f -0.3 -o "${name}1$ext"
+python ~/bin/verve/cell-size.py -i $file -f -0.2 -o "${name}2$ext"
+python ~/bin/verve/cell-size.py -i $file -f -0.1 -o "${name}3$ext"
+python ~/bin/verve/cell-size.py -i $file -f 0.0 -o "${name}4$ext"
+python ~/bin/verve/cell-size.py -i $file -f 0.1 -o "${name}5$ext"
+python ~/bin/verve/cell-size.py -i $file -f 0.2 -o "${name}6$ext"
+python ~/bin/verve/cell-size.py -i $file -f 0.3 -o "${name}7$ext"
 
 mkdir 1_-0.3  2_-0.2  3_-0.1  4_0.0  5_+0.1  6_+0.2  7_+0.3
 
