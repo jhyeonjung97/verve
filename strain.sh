b6d1e8b5dbde287c667f1bb6ea699db42922e0eb
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-09 17:45:59 -0800

    .

--- strain.sh
+++ strain.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-if [[ -s './relaxed/restart.json' ]]; then
-    cp relaxed/restart.json .
-elif [[ -s './relaxed/CONTCAR' ]]; then
-    cp relaxed/CONTCAR .
-elif [[ -s './relaxed/start.traj' ]]; then
-    cp relaxed/start.traj .
-fi
+ase convert relaxed/CONTCAR 'start.traj'
     
 mkdir 1_-0.3  2_-0.2  3_-0.1  4_0.0  5_+0.1  6_+0.2  7_+0.3
 
