b719099bccff237dbb881c95b4c07ac4bd331122
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-09 20:22:28 -0800

    .

--- outcar.sh
+++ outcar.sh
@@ -61,7 +61,8 @@ for dir in $DIR
 do
     cd $dir
     dir_pwd=$(pwd)
-    echo -e "\e[0;32m$clr_tag$dir_pwd/$file\e[0m"
+    echo $mag_tag $pattern_s $pattern_e $file $m
+    echo -e "$clr_tag$dir_pwd/$file\e[0m"
     n=$(awk "/$pattern_s/{flag=1;next}/$pattern_e/{if(flag){count++;flag=0}}END{print count}" $file)
     m=$(awk "/$pattern_s/{count=0;flag=1;next}/$pattern_e/{if(flag){print count;flag=0}}flag{count++}" $file | tail -n 1)
     awk "/$pattern_s/,/$pattern_e/" $file | tail -n $(($m+2))
