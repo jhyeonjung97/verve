72230e278c6cd23be1e099eac8569d6ed8a40c44
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-13 01:30:57 -0700

    .

--- mmff-result.sh
+++ mmff-result.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 grep -B 1 "CONVERGED" err.*.log | grep -v "CONVERGED" | grep -vE "^--$" > temp.out
+awk '$1 <= 29' temp.out > filtered_temp.out && mv filtered_temp.out temp.out
 awk '{printf "%s %s\n", (NR<10 ? "0" NR : NR), $2}' temp.out | sort -k2,2n > mmff.out
 
 rm temp.out
