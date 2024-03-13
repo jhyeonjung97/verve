4898187a888076ac2f709051ea7d81397ad2b2ea
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-12 21:48:49 -0700

    .

---
+++ .ipynb_checkpoints/alias-mac-checkpoint.sh
@@ -0,0 +1,12 @@
+alias jl='cd ~/bin; jupyter-lab'
+alias jn='ssh -N -f -L localhost:7777:localhost:9999 hyeonjung@burning.postech.ac.kr -p 54329; jupyter notebook'
+
+alias cdw='cd ~/Desktop/'
+alias vbash='vi ~/.zshrc'
+alias sbash='source ~/.zshrc'
+alias port='scp -P 54329 hyeonjung@burning.postech.ac.kr:~/port/*.vasp .'
+
+alias vasp2png='python ~/bin/shoulder/vasp2png.py'
+alias style='open /Applications/VESTA/VESTA.app/Contents/Resources/style.ini'
+alias element='open /Applications/VESTA/VESTA.app/Contents/Resources/elements.ini'
+alias carbon='sed -i -e "s/1.89002  0  2/1.89002  0  0/" *.vesta'
