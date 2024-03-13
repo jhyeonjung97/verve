3dfed18475857ea0faf3a7d0339d432b23657108
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-13 15:35:41 -0700

    .

--- err.sh
+++ err.sh
@@ -1,7 +1,7 @@
 if [[ $1 == '-r' ]]; then
-    grep '\-\-\-\-\-\-\-\-\-\-\-\-' */vasp.out
+    grep '\-\-\-\-\-\-\-\-\-\-\-\-' */vasp.out | tail -n 1
     tail */err.*.log
 else
-    grep '\-\-\-\-\-\-\-\-\-\-\-\-' vasp.out
+    grep '\-\-\-\-\-\-\-\-\-\-\-\-' vasp.out | tail -n 1
     tail err.*.log
 fi
