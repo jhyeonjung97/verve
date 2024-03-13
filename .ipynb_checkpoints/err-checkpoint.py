c91daf91d5295840bfbec286127b993b79e03eb8
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-13 14:39:24 -0700

    .

--- .ipynb_checkpoints/err-checkpoint.py
+++ .ipynb_checkpoints/err-checkpoint.py
@@ -1,10 +1,8 @@
 import re
 
 pattern = re.compile(r'--------------')
-# error_found = False
 with open('./vasp.out', 'r') as file:
     for line in file:
         if pattern.search(line):
             print('Warning: Error found in any vasp.out files.')
-            # error_found = True
             break
