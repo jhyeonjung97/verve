dd9ce3dd672423115290b9eac5ad797ab1fb694b
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-13 00:45:09 -0700

    .

---
+++ cation.sh
@@ -0,0 +1,5 @@
+for i in {0..7}
+do
+    mkdir $i
+    cp cation*_$i.traj $i/start.traj
+done
--- mmff-result.sh
+++ mmff-result.sh
@@ -2,3 +2,6 @@
 
 grep -B 1 "CONVERGED" err.*.log | grep -v "CONVERGED" | grep -vE "^--$" > temp.out
 awk '{printf "%s %s\n", (NR<10 ? "0" NR : NR), $2}' temp.out | sort -k2,2n > mmff.out
+
+rm temp.out
+more mmff.out
