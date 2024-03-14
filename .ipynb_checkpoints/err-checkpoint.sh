649b4376a405dd8c4df68f0b419778e128f2fe15
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-14 15:20:50 -0700

    .

--- .ipynb_checkpoints/err-checkpoint.sh
+++ .ipynb_checkpoints/err-checkpoint.sh
@@ -10,7 +10,7 @@ else
             python ~/bin/verve/err-mag.py
             if grep -q '\-\-\-\-\-\-\-\-\-\-\-\-' vasp.out; then
                 echo -n -e "\e[35m$dir\e[0m"
-                grep '\-\-\-\-\-\-\-\-\-\-\-\-' $dir'vasp.out' | tail -n 1
+                grep '\-\-\-\-\-\-\-\-\-\-\-\-' vasp.out | tail -n 1
             fi
         fi
         cd $dir_now
