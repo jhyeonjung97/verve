76496cd565e2fdd8aeb06fa75fe4cc2727338ebc
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-14 09:45:06 -0700

    .

--- err.sh
+++ err.sh
@@ -1,10 +1,12 @@
 if [[ $1 == '-r' ]]; then
     dir_now=$PWD
     for dir in */
+    do
         cd $dir
         grep '\-\-\-\-\-\-\-\-\-\-\-\-' */vasp.out | tail -n 1
         tail */err.*.log
         cd $dir_now
+    done
 else
     grep '\-\-\-\-\-\-\-\-\-\-\-\-' vasp.out | tail -n 1
     tail err.*.log
