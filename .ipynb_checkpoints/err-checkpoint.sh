741a2bb478fb1f0b8af9714afa80f03f9082d4e9
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-14 15:23:29 -0700

    .

--- .ipynb_checkpoints/err-checkpoint.sh
+++ .ipynb_checkpoints/err-checkpoint.sh
@@ -1,3 +1,4 @@
+err_count=0
 if [[ $1 == '-r' ]]; then
     grep '\-\-\-\-\-\-\-\-\-\-\-\-' vasp.out | tail -n 1
     tail err.*.log
@@ -11,12 +12,16 @@ else
             if grep -q '\-\-\-\-\-\-\-\-\-\-\-\-' vasp.out; then
                 echo -n -e "\e[35m$dir\e[0m"
                 grep '\-\-\-\-\-\-\-\-\-\-\-\-' vasp.out | tail -n 1
+                err_count=1
             fi
         fi
         cd $dir_now
         if [[ -s err.*.long ]]; then
             echo -n -e "\e[35m$dir\e[0m" && tail vasp.out | tail -n 1
+            err_count=1
         fi
     done
-    # tail */err.*.log
+    if [[ $err_count == 0 ]]; then
+        echo "\e[34mCongrats! There is no error founded in vasp.out files\e[0m"
+    fi
 fi
