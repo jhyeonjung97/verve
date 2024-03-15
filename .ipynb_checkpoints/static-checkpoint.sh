30cf12e86bbfcd07c60821441529681cf2d6cf33
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-14 18:19:35 -0700

    .

---
+++ .ipynb_checkpoints/static-checkpoint.sh
@@ -0,0 +1,18 @@
+dir_now=$PWD
+for dir in *_*/
+do
+    cd $dir
+    if [[ -d 'opt' ]]; then
+        echo 'There is already the directory named opt..'
+        exit 1
+    else
+        mkdir opt
+        cp * opt/
+        if [[ -d conti* ]]; then
+            mv conti*/ opt/
+        fi
+        rm *.log
+    fi
+    cd $dir_now
+done
+echo -e "\e[32mDon't forget to 1) spread correct submit.sh, 2) name -rc, 3) sub -r\e[0m"
