bf159e276dadf1ec89ec8edffa022407295442de
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-15 13:45:51 -0700

    .

---
+++ hour.sh
@@ -0,0 +1,6 @@
+if [[ $1 -lt 10 ]]; then
+    time='0'$1
+else
+    time=$1
+fi
+sed -i "/#SBATCH -t/c\#SBATCH -t $time:00:00" submit.sh
---
+++ minute.sh
@@ -0,0 +1,6 @@
+if [[ $1 -lt 10 ]]; then
+    time='0'$1
+else
+    time=$1
+fi
+sed -i "/#SBATCH -t/c\#SBATCH -t 00:$time:00" submit.sh
