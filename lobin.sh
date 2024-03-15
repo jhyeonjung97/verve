92797e788bb6cf687bfcac7c4ea6576e6f855258
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-15 13:58:55 -0700

    .

--- lobin.sh
+++ lobin.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+dir_now=$PWD
 cp ~/bin/shoulder/lobsterin .
 sh ~/bin/verve/spread.sh lobsterin
 for dir in *_*/; do
+    cd $dir
     dir=${dir%/}
     IFS='_' read -r A B <<< "$dir"
     sed -i -e "s/X/$B/g" lobsterin
+    cd $dir_now
 done
