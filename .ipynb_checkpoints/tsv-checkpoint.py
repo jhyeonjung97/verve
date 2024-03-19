e8eef5807f34c6042d17cb52bc3963062eb7420d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-19 16:42:15 -0700

    .

--- .ipynb_checkpoints/tsv-checkpoint.py
+++ .ipynb_checkpoints/tsv-checkpoint.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import sys
 import argparse
 
-def plot_patterns_from_multiple_tsv(filenames, png_filename, xlabel, ylabel):
+def plot_patterns_from_multiple_tsv(filenames, png_filename, xlabel, ylabel, labels):
     """
     Reads multiple TSV files, where the first column of each file indicates patterns and the first row indicates elements.
     Then, plots each pattern across elements from all files.
@@ -12,8 +12,8 @@ def plot_patterns_from_multiple_tsv(filenames, png_filename, xlabel, ylabel):
     - filenames: List of filenames of the TSV files.
     """
     plt.figure(figsize=(14, 8))
-    for file in filenames:
-        label = file.split('/')[-1].replace('.tsv', '')
+    for i, file in enumerate(filenames):
+        label = labels[i] if labels and i < len(labels) else file.split('/')[-1].replace('.tsv', '')
         df = pd.read_csv(file, delimiter='\t', index_col=0).T
         for pattern in df.columns:
             plt.plot(df.index, df[pattern], marker='o', linestyle='-', label=f"{label}")
@@ -35,6 +35,7 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--output', dest='filename', type=str, default='merged', help="The filename for the output PNG file.")
     parser.add_argument('-x', '--xlabel', type=str, default='Element or Lattice parameter (Å)', help="xlabel")
     parser.add_argument('-y', '--ylabel', type=str, default='Energy (eV) or Charge (e)', help="ylabel")
+    parser.add_argument('-l', '--labels', nargs='*', help="Custom labels for each file", default=None)
     args = parser.parse_args()
     png_filename = f"merged_{args.filename}.png"    
     plot_patterns_from_multiple_tsv(args.files, png_filename, args.xlabel, args.ylabel)
