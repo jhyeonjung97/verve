0f4899b106d16eb5fb0ac6fa1c7d787f43e20de4
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-19 16:29:45 -0700

    .

--- energy.py
+++ energy.py
@@ -220,7 +220,7 @@ def extract_values(directory, patterns, dir_range, outcar):
                     if match_c:
                         c = float(match_c.group(1))
                 values.setdefault('hexa_ratio', []).append(c / a)
-                values.setdefault('hexa_ratio_Wurtzite', []).append(1.63907194282411056808)
+                values.setdefault('hexa_ratio0', []).append(1.633)
                         
         if patterns:
             outcar_path = os.path.join(dir_path, outcar)
