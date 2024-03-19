0c1d5928d65fe094e6ccd3a0489430dcd01c8818
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-19 10:44:19 -0700

    .

--- tsv.py
+++ tsv.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 
-def plot_patterns_from_multiple_tsv(filenames):
+def plot_patterns_from_multiple_tsv(filenames, png_filename, xlabel, ylabel):
     """
     Reads multiple TSV files, where the first column of each file indicates patterns and the first row indicates elements.
     Then, plots each pattern across elements from all files.
@@ -17,16 +17,22 @@ def plot_patterns_from_multiple_tsv(filenames):
         for pattern in df.columns:
             plt.plot(df.index, df[pattern], marker='o', linestyle='-', label=f"{label}")
     
-    plt.title('Property Values Across Elements from Multiple Files')
-    plt.xlabel('Elements')
-    plt.ylabel('Property Value')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.xticks(rotation=45)
     plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.tight_layout()
-    plt.show()
+    plt.gcf().savefig(png_filename, bbox_inches="tight")
+    print(f"Figure saved as {png_filename}")
+    plt.close()
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        plot_patterns_from_multiple_tsv(sys.argv[1:])
-    else:
-        print("Please specify the path to the TSV files.")
+    parser = argparse.ArgumentParser(description='Plot TSV data.')
+    parser.add_argument('files', nargs='+', help='The TSV files to plot.')
+    parser.add_argument('-o', '--output', dest='filename', type=str, default='merged', help="The filename for the output PNG file.")
+    parser.add_argument('-x', '--xlabel', type=str, default='Element or Lattice parameter (Å)', help="xlabel")
+    parser.add_argument('-y', '--ylabel', type=str, default='Energy (eV) or Charge (e)', help="ylabel")
+    args = parser.parse_args()
+    png_filename = f"{args.filename}.png"    
+    plot_patterns_from_multiple_tsv(args.files, png_filename, args.xlabel, args.ylabel)
+
