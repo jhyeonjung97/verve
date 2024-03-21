6c30c8190621d5264f94e6db38b6eeb0eca7571d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-21 16:13:47 -0700

    .

--- .ipynb_checkpoints/tsv-checkpoint.py
+++ .ipynb_checkpoints/tsv-checkpoint.py
@@ -15,7 +15,7 @@ def plot_patterns_from_multiple_tsv(filenames, png_filename, xlabel, ylabel, lab
     all_indexes = []
     for file in filenames:
         df = pd.read_csv(file, delimiter='\t', index_col=0).T
-        all_indexes.extend(df.index)
+        all_indexes.extend([df.index])
     print(all_indexes)
     # merged_index = pd.Index(np.unique(all_indexes)).sort_values()
    
