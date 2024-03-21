4a9ed2cec2fef2ddf061a47f20c4c40d3ad28ea1
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-21 16:36:37 -0700

    .

--- .ipynb_checkpoints/tsv-checkpoint.py
+++ .ipynb_checkpoints/tsv-checkpoint.py
@@ -12,34 +12,65 @@ def plot_patterns_from_multiple_tsv(filenames, png_filename, xlabel, ylabel, lab
     - filenames: List of filenames of the TSV files.
     """
     plt.figure(figsize=(14, 8))
-    all_indices = []
+    all_indices_sets = []
+    longest_length = 0
     for file in filenames:
         df = pd.read_csv(file, delimiter='\t', index_col=0).T
+        if len(df.index.tolist()) > longest_length:
+            longest_length = current_length
         indices_tuple = tuple(df.index)
-        all_indices.append(indices_tuple)
+        all_indices_sets.append(indices_tuple)
+        
     seen = set()
     unique_indices_sets = []
-    for indices in all_indices:
+    for indices in all_indices_sets:
         if indices not in seen:
             unique_indices_sets.append(indices)
             seen.add(indices)
+
+    merged_indices = ['' for _ in range(longest_length)]
+    # Iterate through each position up to the longest_length
+    for j in range(longest_length):
+        # Concatenate the index label at position j from each unique set, if available
+        for indices in unique_indices_sets:
+            if j < len(indices):  # Check if j is within the bounds of the current set of indices
+                merged_indices[j] += str(indices[j]) + " "  # Concatenate with a space for readability
+    final_indices = pd.Index([index.strip() for index in merged_indices])
+    
+    print(final_indices)
+    # for i, indices in enumerate(unique_indices_sets):
+    #     for j in range(1, longest_length):
+    #         merged_indices[j] += str(indices[i][j])
+    # final_indices = pd.Index(merged_indices)
+    
+
+    for file in filenames:
+        df = pd.read_csv(file, delimiter='\t', index_col=0).T
+        current_indices = df.index.tolist()  # Convert the current set of indices to a list
+        current_length = len(current_indices)  # Find the length of the current set of indices
+        
+        # If the current set of indices is longer than any we've seen before, update our records
+        if current_length > longest_length:
+            longest_indices = current_indices
+            longest_length = current_length
+        
     print(unique_indices_sets)
     # 
    
-    # for i, file in enumerate(filenames):
-    #     label = labels[i] if labels and i < len(labels) else file.split('/')[-1].replace('.tsv', '')
-    #     df = pd.read_csv(file, delimiter='\t', index_col=0).T
-    #     for pattern in df.columns:
-    #         plt.plot(merged_index, df[pattern], marker='o', linestyle='-', label=f"{label}")
-    # plt.xlabel(xlabel)
-    # plt.ylabel(ylabel)
-    # plt.xticks(merged_index, rotation=45)
-    # # plt.xticks(np.arange(len(dir_names)), dir_names, rotation='vertical')
-    # # plt.grid(True)
-    # plt.legend()
-    # plt.tight_layout()
-    # plt.gcf().savefig(png_filename, bbox_inches="tight")
-    # print(f"Figure saved as {png_filename}")
+    for i, file in enumerate(filenames):
+        label = labels[i] if labels and i < len(labels) else file.split('/')[-1].replace('.tsv', '')
+        df = pd.read_csv(file, delimiter='\t', index_col=0).T
+        for pattern in df.columns:
+            plt.plot(merged_index, df[pattern], marker='o', linestyle='-', label=f"{label}")
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.xticks(merged_index, rotation=45)
+    # plt.xticks(np.arange(len(dir_names)), dir_names, rotation='vertical')
+    # plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
+    plt.gcf().savefig(png_filename, bbox_inches="tight")
+    print(f"Figure saved as {png_filename}")
     plt.close()
 
 if __name__ == "__main__":
