0e9fd023b000f20dbb0fe21e2612ead1c114e8ff
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-21 15:10:52 -0700

    .

---
+++ untitled.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp --include="*/" --include="*.png" --exclude="*" jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/2_icohp/1_octahedron/1_high_spin/mag .
