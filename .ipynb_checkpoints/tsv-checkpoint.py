a91bdfcc26b6762fec09789e22ce7bef447a591c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-21 16:13:01 -0700

    .

--- .ipynb_checkpoints/tsv-checkpoint.py
+++ .ipynb_checkpoints/tsv-checkpoint.py
@@ -15,27 +15,24 @@ def plot_patterns_from_multiple_tsv(filenames, png_filename, xlabel, ylabel, lab
     all_indexes = []
     for file in filenames:
         df = pd.read_csv(file, delimiter='\t', index_col=0).T
-        all_indexes.extend(df.index.astype(float))
-        
-    merged_index = pd.Index(np.unique(all_indexes)).sort_values()
+        all_indexes.extend(df.index)
+    print(all_indexes)
+    # merged_index = pd.Index(np.unique(all_indexes)).sort_values()
    
-    for i, file in enumerate(filenames):
-        label = labels[i] if labels and i < len(labels) else file.split('/')[-1].replace('.tsv', '')
-        df = pd.read_csv(file, delimiter='\t', index_col=0).T
-        # df.index = df.index.astype(float)
-        df_interpolated = df.reindex(df.index.union(merged_index)).interpolate(method='index').reindex(merged_index)
-        for pattern in df.columns:
-            plt.plot(merged_index, df_interpolated[pattern], df[pattern], 
-                     arker='o', linestyle='-', label=f"{label}")
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.xticks(merged_index, rotation=45)
-    # plt.xticks(np.arange(len(dir_names)), dir_names, rotation='vertical')
-    # plt.grid(True)
-    plt.legend()
-    plt.tight_layout()
-    plt.gcf().savefig(png_filename, bbox_inches="tight")
-    print(f"Figure saved as {png_filename}")
+    # for i, file in enumerate(filenames):
+    #     label = labels[i] if labels and i < len(labels) else file.split('/')[-1].replace('.tsv', '')
+    #     df = pd.read_csv(file, delimiter='\t', index_col=0).T
+    #     for pattern in df.columns:
+    #         plt.plot(merged_index, df[pattern], marker='o', linestyle='-', label=f"{label}")
+    # plt.xlabel(xlabel)
+    # plt.ylabel(ylabel)
+    # plt.xticks(merged_index, rotation=45)
+    # # plt.xticks(np.arange(len(dir_names)), dir_names, rotation='vertical')
+    # # plt.grid(True)
+    # plt.legend()
+    # plt.tight_layout()
+    # plt.gcf().savefig(png_filename, bbox_inches="tight")
+    # print(f"Figure saved as {png_filename}")
     plt.close()
 
 if __name__ == "__main__":
