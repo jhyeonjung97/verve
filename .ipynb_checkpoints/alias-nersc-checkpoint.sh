18d25c12e08cd264374bea65522d7bdd81a3b569
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-22 11:56:37 -0700

    .

--- .ipynb_checkpoints/alias-nersc-checkpoint.sh
+++ .ipynb_checkpoints/alias-nersc-checkpoint.sh
@@ -7,6 +7,6 @@ alias vasp5='mv /global/homes/j/jiuy97/bin/vasp_outcar_parsers5.py /global/homes
 alias vasp6='mv /global/homes/j/jiuy97/bin/vasp_outcar_parsers6.py /global/homes/j/jiuy97/.local/lib/python3.11/site-packages/ase/io/vasp_parsers/vasp_outcar_parsers.py'
 alias mystat='squeue -o "%.10i %.9P %.16j %.8u %.8T %.8M %.10l %.6D %.15R" --me --sort=i'
 alias idle='squeue -o "%.10i %.9P %.16j %.8u %.8T %.8M %.10l %.6D %.15R" --me --sort=i | grep "30:00" | grep gpu
-squeue -o "%.10i %.9P %.16j %.8u %.8T %.8M %.10l %.6D %.15R" --me --sort=i | grep "30:00" | grep cpu'
+squeue -o "%.10i %.9P %.16j %.8u %.8T %.8M %.10l %.6D %.15R" --me --sort=i | grep "30:00" | grep regular'
 
 alias qdel='scancel'
