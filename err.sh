ad8ec9998db011c4b68e7dc618ff62fd3b8dbb10
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-25 10:10:54 -0700

    .

--- err.sh
+++ err.sh
@@ -38,6 +38,7 @@ else
             for file in err.*.log
             do
                 sed -i '/Lmod is automatically replacing/d' $file
+                sed -i '/^$/d' $file
                 if [[ -s $file ]]; then
                     echo -n -e "\e[35m$dir\e[0m"
                     tail $file | tail -n 2
@@ -55,6 +56,7 @@ else
                 for file in opt/err.*.log
                 do
                     sed -i '/Lmod is automatically replacing/d' $file
+                    sed -i '/^$/d' $file
                     if [[ -s $file ]]; then
                         echo -n -e "\e[35m$dir\e[0m"
                         tail $file | tail -n 2
