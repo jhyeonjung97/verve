56f7509482cecb77cb42ef07862ac6b4b1fc6093
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-26 09:54:27 -0700

    .

---
+++ png.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+/usr/bin/rsync -e 'ssh' --ignore-times --size-only -avlzp -K --max-size=50000m \
+	--include="*/" \
+	--include="*.png" \
+	--exclude="*" jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/3_V_shape .
