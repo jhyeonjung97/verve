3958c741abc8ae1ec061c9e2269efa397d9224e8
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-26 01:05:21 -0700

    .

--- .ipynb_checkpoints/sumup-checkpoint.py
+++ .ipynb_checkpoints/sumup-checkpoint.py
@@ -49,9 +49,7 @@ def line_fitting(xfiles, yfiles, xlabel, ylabel, png_filename, tsv_filename):
     #          f"R^2: {R2:.3f}, MAE: {MAE:.3f}, MSE: {MSE:.3f}", fontsize=12)
     
     x_text_margin = np.min(X) + (np.max(X) - np.min(X)) * 0.02
-    y_text_margin_top = np.max(Y) - (np.max(Y) - np.min(Y)) * 0.05
-    y_text_margin_bottom = y_text_margin_top - (np.max(Y) - np.min(Y)) * 0.1
-    print(x_text_margin, y_text_margin_top, y_text_margin_bottom)
+    y_text_margin = np.max(Y) - (np.max(Y) - np.min(Y)) * 0.05
 
     plt.figure()
     plt.scatter(X, Y, color='r')
@@ -62,10 +60,8 @@ def line_fitting(xfiles, yfiles, xlabel, ylabel, png_filename, tsv_filename):
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     
-    plt.text(x_text_margin, y_text_margin_top, 
-             f"Y = {a:.3f}X + {b:.3f}", fontsize=12)
-    plt.text(x_text_margin, y_text_margin_bottom, 
-             f"R^2: {R2:.3f}, MAE: {MAE:.3f}, MSE: {MSE:.3f}", fontsize=12)
+    plt.text(x_text_margin, y_text_margin, 
+             f"Y = {a:.3f}X + {b:.3f}\nR^2: {R2:.3f}, MAE: {MAE:.3f}, MSE: {MSE:.3f}", fontsize=12)
     
     plt.savefig(png_filename, bbox_inches="tight")
     print(f"Figure saved as {png_filename}")
