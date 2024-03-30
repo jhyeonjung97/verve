aa9bc22194f1106588b761eea630b69ca502775a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-29 17:47:56 -0700

    .

--- mpi.py
+++ mpi.py
@@ -14,15 +14,15 @@ api_key = '3jSckrUWEJ94DEo93ZOlCwNd2B1BHerV'
 adaptor = AseAtomsAdaptor()
 
 with MPRester(api_key) as mpr:
-    for material_id in args.ids:
+    for id in args.ids:
         try:
-            search_results = mpr.materials.get_data_by_id(material_id=material_id, fields=['structure'])
+            search_results = mpr.materials.get_data_by_id(material_id=f'mp-{id}', fields=['structure'])
             if search_results:
                 atoms = adaptor.get_atoms(search_results.structure)
-                filename = os.path.join("./", f'{material_id}.traj')
+                filename = os.path.join("./", f'mp-{id}.traj')
                 write(filename, atoms)
-                print(f"Saved {material_id}.traj")
+                print(f"Saved mp-{id}.traj")
             else:
-                print(f"No data found for material ID: {material_id}")
+                print(f"No data found for material ID: mp-{id}")
         except Exception as e:
-            print(f"Error fetching data for material ID {material_id}: {e}")
+            print(f"Error fetching data for material ID mp-{id}: {e}")
