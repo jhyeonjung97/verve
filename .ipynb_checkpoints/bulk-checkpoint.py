205209a9819f7c3bb379b5a59c80c9919b9bbf4d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-29 17:04:48 -0700

    .

--- .ipynb_checkpoints/bulk-checkpoint.py
+++ .ipynb_checkpoints/bulk-checkpoint.py
@@ -1,25 +1,36 @@
-from pymatgen.ext.matproj import MPRester
-from pymatgen.core.structure import Structure
-
-# Your Materials Project API Key
-API_KEY = '3jSckrUWEJ94DEo93ZOlCwNd2B1BHerV'
-
-metal_rows = {
-    '3d': ['Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge'],
-    '4d': ['Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn'],
-    '5d': ['Ba', 'La', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb'],
-    }
-
-def get_zero_hull_energy_materials(api_key):
-    with MPRester(api_key) as m:
-        materials = m.materials.summary.search(e_above_hull=0, fields=['material_id', 'formula', 'structure'])
-        return materials
+from mp_api.client import MPRester
+from pymatgen.io.ase import AseAtomsAdaptor
+from ase.io import write
+import subprocess
 
 def main():
-    materials = get_zero_hull_energy_materials(API_KEY)
-    for material in materials:
-        print(f"Material ID: {material['material_id']}, Formula: {material['formula']}")
-        print("Structure:", material['structure'])
+    api_key = '3jSckrUWEJ94DEo93ZOlCwNd2B1BHerV'
+    metal_rows = {
+        '3d': ['Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge'],
+        '4d': ['Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn'],
+        '5d': ['Ba', 'La', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb'],
+    }
+        
+    materials = get_zero_hull_energy_materials(api_key, metal_rows)
+        
+def get_zero_hull_energy_materials(api_key, metal_rows):
+    adaptor = AseAtomsAdaptor()
+    with MPRester(api_key) as mpr:
+        for row in metal_rows:
+            row_dir = f"./{row}"
+            os.makedirs(row_dir, exist_ok=True)
+            for element in metal_rows[row]:
+                element_dir = os.path.join(row_dir, element)
+                os.makedirs(element_dir, exist_ok=True)
+                
+                search_results = mpr.materials.summary.search(chemsys=element, energy_above_hull=(0, 0), fields=['structure'])
+                if len(search_results) == 1:
+                    atoms = adaptor.get_atoms(search_results[0].structure)
+                    filename = os.path.join(element_dir, 'start.traj')
+                    write(filename, atoms)
+                    print(f"Saved {filename}")
+                else:
+                    raise ValueError(f'There are more than one material of {element}')                    
 
 if __name__ == "__main__":
     main()
---
+++ bulk.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+file=$1
+case "$file" in
+    POSCAR|CONTCAR|*.vasp)
+        ;;
+    *)
+        echo 'Are you sure..? Give me POSCAR or CONTCAR or *.vasp'
+        exit 1
+        ;;
+esac
+
+if [[ -z $2 ]]; then
+    pattern='X'
+else
+    pattern=$2
+fi
+
+metals_3d=(Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge)
+metals_4d=(Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn)
+metals_5d=(Ba La Hf Ta W Re Os Ir Pt Au Hg Tl Pb)
+
+mkdir -p 3d 4d 5d
+
+process_metals() {
+    local -n metals=$1
+    local subdir=$2
+    cd $subdir
+    for i in ${!metals[@]}; do
+        local formatted_index=$(printf "%02d" $i)
+        local dir="${formatted_index}_${metals[i]}"
+        mkdir $dir
+        sed "s/$pattern/${metals[i]}/" ../$file > $dir/$file
+        echo "sed \"s/$pattern/${metals[i]}/\" ../$file > $dir/$file"
+    done
+    cd ..
+}
+
+process_metals metals_3d 3d
+process_metals metals_4d 4d
+process_metals metals_5d 5d
+rm $file
+
+sed -i -e "/^[^#]/s/^/#/" ~/bin/temp.sh
+echo "ase convert -f $file start.traj" >> ~/bin/temp.sh
+echo "rm $file" >> ~/bin/temp.sh
+sh ~/bin/verve/temp.sh -rr
+sed -i -e '/^[^#]/d' ~/bin/temp.sh
