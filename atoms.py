e0b470a10ee21292a38704b57fdcf4a2f33b93d1
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-04-01 15:38:35 -0700

    .

--- atoms.py
+++ atoms.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--magnetic', action='store_true', default=False, help='Print magnetic moments')
 parser.add_argument('-c', '--charge', action='store_true', default=False, help='Print Bader charges')
 parser.add_argument('-e', '--energy', action='store_true', default=False, help='Print total energy')
+parser.add_argument('-f', '--formula', action='store_true', default=False, help='Print chemical formula')
 args = parser.parse_args()
 
 class Colors:
@@ -36,6 +37,8 @@ for dir in sorted_dirs:
             print(f"{Colors.CYAN}{dir}{Colors.RESET}", atoms.get_magnetic_moments())
         if args.energy:
             print(f"{Colors.ORANGE}{dir}{Colors.RESET}", atoms.get_total_energy())
+        if args.formula:
+            print(f"{Colors.MAGENTA}{dir}{Colors.RESET}", atoms.get_chemical_symbols())
         if args.charge:
             chg_path = os.path.join(dir_path, 'atoms_bader_charge.json')
             if not os.path.exists(chg_path):
