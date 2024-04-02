a0cd055f877d2c4f3a75e07077e4d28a572e790f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-04-01 19:32:30 -0700

    .

--- .ipynb_checkpoints/sumup-checkpoint.py
+++ .ipynb_checkpoints/sumup-checkpoint.py
@@ -25,13 +25,13 @@ def line_fitting(xfiles, yfiles, xlabel, ylabel, png_filename, tsv_filename):
 
     num_rows, num_cols = summed_x.shape
     plt.figure()
-    XX_values = None
-    YY_values = None
+    XX_values = np.array([])
+    YY_values = np.array([])
     for col_name in summed_x.columns:
         X_values = summed_x[col_name].values
         Y_values = summed_y[col_name].values
-        XX_values += X_values
-        YY_values += Y_values
+        XX_values = np.concatenate((XX_values, X_values))
+        YY_values = np.concatenate((YY_values, Y_values))
         plt.scatter(X_values, Y_values, label=col_name)
         
     A = np.vstack([XX_values, np.ones(len(XX_values))]).T
