8fdd831b2ef166dc2da4ab6c22b20439c4ccc2f5
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-04-01 19:35:52 -0700

    .

--- sumup.py
+++ sumup.py
@@ -23,10 +23,11 @@ def line_fitting(xfiles, yfiles, xlabel, ylabel, png_filename, tsv_filename):
         else:
             summed_y += df
 
-    num_rows, num_cols = summed_x.shape
     plt.figure()
     XX_values = np.array([])
     YY_values = np.array([])
+    num_rows, num_cols = summed_x.shape
+    
     for col_name in summed_x.columns:
         X_values = summed_x[col_name].values
         Y_values = summed_y[col_name].values
@@ -39,51 +40,36 @@ def line_fitting(xfiles, yfiles, xlabel, ylabel, png_filename, tsv_filename):
     a, b = coeffs
     xx = np.linspace(np.min(XX_values), np.max(XX_values), 1000)
     yy = a * xx + b
-    plt.plot(xx, yy, color='b', alpha=0.5)
+    
+    plt.plot(xx, yy, color='black', alpha=0.5)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    
+    YY_pred = a*XX_values + b
+    R2 = r2_score(YY_values, YY_pred)
+    MAE = mean_absolute_error(YY_values, YY_pred)
+    MSE = mean_squared_error(YY_values, YY_pred)
 
-    plt.xlabel('summed_x')
-    plt.ylabel('summed_y')
+    plt.text(x_text_margin, y_text_margin, 
+             f"Y = {a:.3f}X + {b:.3f}\nR^2: {R2:.3f}, MAE: {MAE:.3f}, MSE: {MSE:.3f}", fontsize=9)
+    
+    # print(f"Y = {a:.3f}X + {b:.3f}")
+    # print(f"R^2: {R2:.3f}, MAE: {MAE:.3f}, MSE: {MSE:.3f}")
+    # plt.text(np.min(X), np.max(Y), f"Y = {a:.3f}X + {b:.3f}", fontsize=12)
+    # plt.text(np.min(X), np.max(Y) - (np.max(Y) - np.min(Y)) * 0.1, 
+    #          f"R^2: {R2:.3f}, MAE: {MAE:.3f}, MSE: {MSE:.3f}", fontsize=12)
+    
+    x_text_margin = np.min(X) + (np.max(X) - np.min(X)) * 0.02
+    y_text_margin = np.max(Y) - (np.max(Y) - np.min(Y)) * 0.05
     
     png_filename = 'scatter_plot.png'
     plt.savefig(png_filename, bbox_inches="tight")
     print(f"Figure saved as {png_filename}")
     plt.close()
+    
 
-    # X = summed_x.values
-    # Y = summed_y.values
-    # A = np.vstack([X, np.ones(len(X))]).T
-    # coeffs, residuals, rank, s = np.linalg.lstsq(A, Y, rcond=None)
-    # a, b = coeffs
-    
-    # Y_pred = a*X + b
-    # R2 = r2_score(Y, Y_pred)
-    # MAE = mean_absolute_error(Y, Y_pred)
-    # MSE = mean_squared_error(Y, Y_pred)
 
-    # # print(f"Y = {a:.3f}X + {b:.3f}")
-    # # print(f"R^2: {R2:.3f}, MAE: {MAE:.3f}, MSE: {MSE:.3f}")
-    # # plt.text(np.min(X), np.max(Y), f"Y = {a:.3f}X + {b:.3f}", fontsize=12)
-    # # plt.text(np.min(X), np.max(Y) - (np.max(Y) - np.min(Y)) * 0.1, 
-    # #          f"R^2: {R2:.3f}, MAE: {MAE:.3f}, MSE: {MSE:.3f}", fontsize=12)
-    
-    # x_text_margin = np.min(X) + (np.max(X) - np.min(X)) * 0.02
-    # y_text_margin = np.max(Y) - (np.max(Y) - np.min(Y)) * 0.05
 
-    # plt.figure()
-    # plt.scatter(X, Y, color='r')
-    # xx = np.linspace(np.min(X), np.max(X), 1000)
-    # yy = a * xx + b
-    
-    # plt.plot(xx, yy, color='b', alpha=0.5)
-    # plt.xlabel(xlabel)
-    # plt.ylabel(ylabel)
-    
-    # plt.text(x_text_margin, y_text_margin, 
-    #          f"Y = {a:.3f}X + {b:.3f}\nR^2: {R2:.3f}, MAE: {MAE:.3f}, MSE: {MSE:.3f}", fontsize=9)
-    
-    # plt.savefig(png_filename, bbox_inches="tight")
-    # print(f"Figure saved as {png_filename}")
-    # plt.close()
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Plot 2d fitting with sumup data.')
