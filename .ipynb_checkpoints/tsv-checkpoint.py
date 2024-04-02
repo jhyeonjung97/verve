201ae3653f601e008966eae1f0d33835ae4d6276
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-04-01 18:52:06 -0700

    .

--- .ipynb_checkpoints/tsv-checkpoint.py
+++ .ipynb_checkpoints/tsv-checkpoint.py
@@ -5,15 +5,16 @@ import sys
 import argparse
 
 def plot_patterns_from_multiple_tsv(filenames, output, xlabel, ylabel, labels, sumup):
-    """
-    Reads multiple TSV files, where the first column of each file indicates patterns and the first row indicates elements.
-    Then, plots each pattern across elements from all files.
+
+    metal_rows = {
+        '3d': ['Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge'],
+        '4d': ['Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn'],
+        '5d': ['Ba', 'La', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb']
+        }
     
-    Parameters:
-    - filenames: List of filenames of the TSV files.
-    """
     merged_df = None
     summed_df = None
+    merged_indice = None
     
     png_filename = f"merged_{output}.png"   
     tsv_filename = f"merged_{output}.tsv"
@@ -24,6 +25,7 @@ def plot_patterns_from_multiple_tsv(filenames, output, xlabel, ylabel, labels, s
 
     colors = ['#d62728', '#ff7f0e', '#2ca02c', '#279ff2', '#9467bd']
     markers = ['s', 'd', 'p', 'o', '>', '<', 'D']
+    
 
     for j, file in enumerate(filenames):
         df = pd.read_csv(file, delimiter='\t').iloc[:, 1:]
@@ -32,27 +34,7 @@ def plot_patterns_from_multiple_tsv(filenames, output, xlabel, ylabel, labels, s
             summed_df = df if summed_df is None else summed_df + df
         else:
             merged_df = pd.concat([merged_df, df], axis=1)
-        # if sumup:
-        #     if summed_df is None:
-        #         summed_df = df.copy()
-        #     else:
-        #         summed_values = summed_df.values + df.values
-        #         summed_df = pd.DataFrame(summed_values, columns=df.columns, index=['Sum'])
-        #         print(summed_df)
-        # else:
-        #     merged_df = pd.concat([merged_df, df], axis=1)
-        #     print(merged_df)
-        #     for pattern in df.columns:
-        #         x = []
-        #         filtered_df = []
-        #         for i, v in enumerate(df[pattern]):
-        #             if not np.isnan(v): 
-        #                 x.append(i)
-        #                 filtered_df.append(v)
-        #         if not filtered_df:
-        #             print(f"No values found for pattern: {pattern}")
-        #             continue
-        #         plt.plot(x, filtered_df, marker=markers[j], color=colors[j], label = labels[j])
+
     if not sumup:
         for j, pattern in enumerate(merged_df.columns):
             x = range(len(merged_df[pattern]))
@@ -63,7 +45,6 @@ def plot_patterns_from_multiple_tsv(filenames, output, xlabel, ylabel, labels, s
             plt.plot(x, filtered_df, marker=markers[j % len(markers)], color=colors[j % len(colors)], label=labels[j % len(labels)])
         if 'hexa_ratio' in df.columns:
             plt.plot(x, [1.633]*len(x), linestyle=':', label='hexa_ratio0', color='black')
-        plt.legend()
     else:
         summed_df.to_csv(sum_filename, sep='\t')
         print(f"Summed data saved to {sum_filename}")
@@ -71,8 +52,12 @@ def plot_patterns_from_multiple_tsv(filenames, output, xlabel, ylabel, labels, s
     
     merged_df.to_csv(tsv_filename, sep='\t')
     print(f"Merged data saved to {tsv_filename}")
-    
-    # plt.xticks(np.arange(len(merged_indices)), merged_indices)
+
+    # for row in metal_rows:
+    #     merged_indices = metal_rows[row] if merged_indices is None else merged_indices + metal_rows[row]
+    for row in metal_rows.values():  # Iterate over the list of metal symbols
+        merged_indices += ' '.join(row) + '\n'
+    plt.xticks(np.arange(len(metal_rows['3d'])), merged_indices)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.legend()
