5af6599fe0fdf0cdb868a92192b7e122c4bcccf4
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-04-02 10:45:00 -0700

    .

--- .ipynb_checkpoints/formation-checkpoint.py
+++ .ipynb_checkpoints/formation-checkpoint.py
@@ -27,7 +27,7 @@ markers = ['s', 'd', 'p', 'o', '>', '<', 'D']
 
 for row in metal_rows:
     oxide_path = './energy_norm_energy.tsv'
-    oxide_df = pd.read_csv(oxide_path, delimiter='\t').iloc[:, 1:]
+    oxide_df = pd.read_csv(oxide_path, delimiter='\t')
     print(oxide_df.index.tolist())
     print(oxide_df.iloc[:, -1])
     if metal_rows[row] == oxide_df.iloc[:, -1]:
