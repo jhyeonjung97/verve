4b460ecce2d867ada9ec8fad72e3903984a7486f
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-04-05 11:51:09 -0700

    .

--- .ipynb_checkpoints/formation-checkpoint.py
+++ .ipynb_checkpoints/formation-checkpoint.py
@@ -54,10 +54,17 @@ ref_df.insert(0, '3d', min_values)
 
 energy_path = './energy_norm_energy.tsv'
 energy_df = pd.read_csv(energy_path, delimiter='\t', index_col=0)
+df = pd.DataFrame(index=energy_df.index, columns=energy_df.columns)
+
+# for row in metal_rows:
+#     if metal_rows[row] == energy_df.index.tolist():
+#         df = energy_df.sub(ref_df[row].values, axis=0) - E_oxygen
+# print(df)
 
 for row in metal_rows:
-    if metal_rows[row] == energy_df.index.tolist():
-        df = energy_df.sub(ref_df[row].values, axis=0) - E_oxygen
+    for metal in metal_rows[row]:
+        if metal in ref_df.index and metal in energy_df.index:
+            df.loc[metal] = energy_df.loc[metal] - ref_df.loc[metal, row] - E_oxygen
 print(df)
 
 # plt.figure(figsize=(8, 6))
