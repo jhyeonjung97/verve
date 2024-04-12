982176ba9b6d26996fd17c53abaf9ed2c8b3498d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-04-12 16:13:36 -0700

    .

---
+++ monoxide.py
@@ -0,0 +1,41 @@
+Ca -634.9 RS
+Sc
+Ti -519.7
+V -431.8
+Cr
+Mn -385.2
+Fe -272.0 RS
+Co
+Ni
+Cu
+Zn -350.5
+Ga
+Ge
+
+Sr
+Y 
+Zr
+Nb
+Mo
+Tc
+Ru
+Rh
+Pd
+Ag
+Cd
+In
+Sn
+
+Ba
+La
+Hf
+Ta
+W 
+Re
+Os
+Ir
+Pt
+Au
+Hg
+Tl
+Pb
