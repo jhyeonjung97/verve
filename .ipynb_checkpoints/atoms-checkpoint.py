0598b3aef98e00be4d2262ffdabf550fd8e0d9c3
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-04-12 10:36:34 -0700

    .

--- .ipynb_checkpoints/atoms-checkpoint.py
+++ .ipynb_checkpoints/atoms-checkpoint.py
@@ -8,7 +8,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--magnetic', action='store_true', default=False, help='Print magnetic moments')
 parser.add_argument('-c', '--charge', action='store_true', default=False, help='Print Bader charges')
 parser.add_argument('-e', '--energy', action='store_true', default=False, help='Print total energy')
-parser.add_argument('-f', '--formula', action='store_true', default=False, help='Print chemical formula')
+parser.add_argument('-a', '--atoms', action='store_true', default=False, help='Print chemical formula')
+parser.add_argument('-f', '--force', action='store_true', default=False, help='Force to select all directories')
 args = parser.parse_args()
 
 class Colors:
@@ -19,7 +20,11 @@ class Colors:
     RED = '\033[91m'
     RESET = '\033[0m'
 
-dirs = [d for d in os.listdir('.') if os.path.isdir(d) and '_' in d]
+if args.force:
+    dirs = [d for d in os.listdir('.') if os.path.isdir(d)]
+else:
+    dirs = [d for d in os.listdir('.') if os.path.isdir(d) and '_' in d]
+    
 sorted_dirs = sorted(dirs)
 for dir in sorted_dirs:
     dir_path = os.path.join('.', dir)
@@ -37,7 +42,7 @@ for dir in sorted_dirs:
             print(f"{Colors.CYAN}{dir}{Colors.RESET}", atoms.get_magnetic_moments())
         if args.energy:
             print(f"{Colors.ORANGE}{dir}{Colors.RESET}", atoms.get_total_energy())
-        if args.formula:
+        if args.atoms:
             print(f"{Colors.MAGENTA}{dir}{Colors.RESET}", atoms.get_chemical_formula())
         if args.charge:
             chg_path = os.path.join(dir_path, 'atoms_bader_charge.json')
