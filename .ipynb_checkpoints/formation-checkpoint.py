b44b4e1cb15fba1cff7dc2aea9822ac722d8cc12
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-04-12 13:39:36 -0700

    .

--- .ipynb_checkpoints/formation-checkpoint.py
+++ .ipynb_checkpoints/formation-checkpoint.py
@@ -87,7 +87,8 @@ for j, column in enumerate(formation.columns):
 formation.to_csv(tsv_filename, sep='\t')
 print(f"Merged data saved to {tsv_filename}")
 
-# plt.xticks(np.arange(len(x)) + 1, x)
+plt.xlim(-0.5, len(x)-0.5)
+plt.xticks(np.arange(len(x)), x)
 plt.xlabel('Metal (MO)')
 plt.ylabel('Formation energy (eV/MO)')
 plt.legend()
