81bdb5969633b81cb79ad461cd40cccf56cad8c6
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-04-12 13:50:30 -0700

    .

--- png.sh
+++ png.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if ${here} == 'slac'; then
+if [[ ${here} == 'slac' ]]; then
     /usr/bin/rsync -e 'ssh' --ignore-times --size-only -avlzp -K --max-size=50000m \
     	--include="*/" \
     	--include="*.png" \
     	--exclude="*" \
      jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/3_V_shape .
-elif ${here} == 'nersc'; then
+elif [[ ${here} == 'nersc' ]]; then
     dir_now='/pscratch/sd/j/jiuy97/3_V_shape'
     cd $dir_now
     for dir in */*/; do
