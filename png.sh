bc7043db9bfe4974701a2b9f715071146e4aeffe
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-04-15 16:16:08 -0700

    .

--- png.sh
+++ png.sh
@@ -18,13 +18,13 @@ elif [[ ${here} == 'nersc' ]]; then
     # cd /pscratch/sd/j/jiuy97/3_V_shape/oxide/0_min
     # python ~/bin/verve/energy.py --save -x "Metal (MxOy)" -y "Total energy (eV/M)" -n m
     # cd $dir_now
-    # for dir in *_*/*/; do
-    #     if [[ $dir != *'save'* ]] && [[ $dir != *'rhom'* ]] && [[ $dir != *'bin'* ]] && [[ $dir != *'cubic'* ]]; then
-    #         cd $dir
+    for dir in *_*/*/; do
+        if [[ $dir != *'save'* ]] && [[ $dir != *'rhom'* ]] && [[ $dir != *'bin'* ]] && [[ $dir != *'cubic'* ]]; then
+            cd $dir
     #         python ~/bin/verve/energy.py --save -p Madelung_L -x "Metal (MO)" -y "Madelugn energy (Loewdin, eV/MO)" -n m
     #         python ~/bin/verve/energy.py --save -p energy -x "Metal (MO)" -y "Total energy (eV/MO)" -n m
     #         python ~/bin/verve/energy.py --save -p volume -x "Metal (MO)" -y "Volume (A^3/MO)" -n m
-    #         python ~/bin/verve/energy.py --save -p mag -e M -x "Metal (MO)" -y "Magnetization"
+            python ~/bin/verve/energy.py --save -p mag -e M -x "Metal (MO)" -y "Magnetization"
     #         python ~/bin/verve/energy.py --save -p GP_L -e M  -x "Metal (MO)" -y "Gross population (Loewdin)"
     #         python ~/bin/verve/formation.py
     #         if [[ $dir == *'Tetrahedral'* ]]; then
@@ -38,8 +38,8 @@ elif [[ ${here} == 'nersc' ]]; then
     #         python ~/bin/verve/energy.py --save -p ICOHP -x "Metal (MO)" -y "ICOHP (eV/M-O)" -n "$n"
     #         sed -i 's/\x0//g' *.tsv
     #         cd $dir_now
-    #     fi
-    # done
+        fi
+    done
     for dir in *_*/; do
         cd $dir
         python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Madelung energy (Loewdin, eV/MO)" -o norm_Madelung_L */energy_norm_Madelung_Loewdin.tsv
