d3258e7ac1bb10dd1404e899add0b75166c6f1ee
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-04-15 13:51:44 -0700

    .

--- .ipynb_checkpoints/png-checkpoint.sh
+++ .ipynb_checkpoints/png-checkpoint.sh
@@ -46,9 +46,9 @@ elif [[ ${here} == 'nersc' ]]; then
         python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Magnetization" -o mag_M */energy_mag_M.tsv
         python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Gross population (Loewdin)" -o magnetization */energy_magnetization.tsv
         python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Formation energy (eV/MO)" -o norm_formation */energy_norm_formation.tsv
-        if [[ $dir == *'Tetraheral'* ]]; then
+        if [[ $PWD == *'Tetraheral'* ]]; then
             python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Hexagonal ratio [c/a]" -o hexa_ratio */energy_hexa_ratio.tsv
-        elif [[ $dir == *'Square_Planar'* ]]; then
+        elif [[ $PWD == *'Square_Planar'* ]]; then
             python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Square prism ratio [c/a]" -o hexa_ratio */energy_hexa_ratio.tsv
         fi
         python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Bond lentgh (A/M-O)" -o norm_bond */energy_norm_bond.tsv
