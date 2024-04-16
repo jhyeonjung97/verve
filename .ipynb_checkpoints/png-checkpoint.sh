a253ac6a4e4a61e4e70263da76b5fcc808570eea
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-04-15 17:49:40 -0700

    .

--- .ipynb_checkpoints/png-checkpoint.sh
+++ .ipynb_checkpoints/png-checkpoint.sh
@@ -42,22 +42,22 @@ elif [[ ${here} == 'nersc' ]]; then
     done
     for dir in *_*/; do
         cd $dir
-        # python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Madelung energy (Loewdin, eV/MO)" -o norm_Madelung_L */energy_norm_Madelung_Loewdin.tsv
-        # python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Total energy (eV/MO)" -o norm_energy */energy_norm_energy.tsv
-        # python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Volume (A^3/MO)" -o norm_volume */energy_norm_volume.tsv
-        # python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Magnetization" -o mag_M */energy_mag_M.tsv
-        # python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Gross population (Loewdin)" -o magnetization */energy_GP_Loewdin_M.tsv
-        # python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Formation energy (eV/MO)" -o norm_formation */energy_norm_formation.tsv
-        # if [[ $PWD == *'Tetraheral'* ]]; then
-        #     python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Hexagonal ratio [c/a]" -o hexa_ratio */energy_hexa_ratio.tsv
-        # elif [[ $PWD == *'Square_Planar'* ]]; then
-        #     python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Square prism ratio [c/a]" -o hexa_ratio */energy_hexa_ratio.tsv
-        # fi
-        # python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Bond lentgh (A/M-O)" -o norm_bond */energy_norm_bond.tsv
-        # python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "ICOHP (eV/M-O)" -o norm_ICOHP */energy_norm_ICOHP.tsv
-        # python ~/bin/verve/sumup.py -x merged_norm_ICOHP.tsv -y merged_norm_formation.tsv --xlabel "ICOHP (eV)" --ylabel "Formation energy (eV)" --xdata ICOHP --ydata formation
-        # python ~/bin/verve/sumup.py -x merged_norm_Madelung_L.tsv -y merged_norm_formation.tsv --xlabel "Madelung_Loewdin (eV)" --ylabel "Formation energy (eV)" --xdata MadelungL --ydata formation
-        # python ~/bin/verve/sumup.py -x merged_norm_ICOHP.tsv merged_norm_Madelung_L.tsv -y merged_norm_formation.tsv --xlabel "ICOHP + Madelung Loewdin (eV)" --ylabel "Formation energy (eV)" --xdata ICOHP+MadelungL --ydata formation            
+        python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Madelung energy (Loewdin, eV/MO)" -o norm_Madelung_L */energy_norm_Madelung_Loewdin.tsv
+        python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Total energy (eV/MO)" -o norm_energy */energy_norm_energy.tsv
+        python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Volume (A^3/MO)" -o norm_volume */energy_norm_volume.tsv
+        python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Magnetization" -o mag_M */energy_mag_M.tsv
+        python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Gross population (Loewdin)" -o magnetization */energy_GP_Loewdin_M.tsv
+        python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Formation energy (eV/MO)" -o norm_formation */energy_norm_formation.tsv
+        if [[ $PWD == *'Tetraheral'* ]]; then
+            python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Hexagonal ratio [c/a]" -o hexa_ratio */energy_hexa_ratio.tsv
+        elif [[ $PWD == *'Square_Planar'* ]]; then
+            python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Square prism ratio [c/a]" -o hexa_ratio */energy_hexa_ratio.tsv
+        fi
+        python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "Bond lentgh (A/M-O)" -o norm_bond */energy_norm_bond.tsv
+        python ~/bin/verve/tsv.py -l 3d_afm 3d_fm 3d 4d 5d -x "Metal (MO)" -y "ICOHP (eV/M-O)" -o norm_ICOHP */energy_norm_ICOHP.tsv
+        python ~/bin/verve/sumup.py -x merged_norm_ICOHP.tsv -y merged_norm_formation.tsv --xlabel "ICOHP (eV)" --ylabel "Formation energy (eV)" --xdata ICOHP --ydata formation
+        python ~/bin/verve/sumup.py -x merged_norm_Madelung_L.tsv -y merged_norm_formation.tsv --xlabel "Madelung_Loewdin (eV)" --ylabel "Formation energy (eV)" --xdata MadelungL --ydata formation
+        python ~/bin/verve/sumup.py -x merged_norm_ICOHP.tsv merged_norm_Madelung_L.tsv -y merged_norm_formation.tsv --xlabel "ICOHP + Madelung Loewdin (eV)" --ylabel "Formation energy (eV)" --xdata ICOHP+MadelungL --ydata formation            
         cd $dir_now
     done
     # python ~/bin/verve/tsv.py -r 3d -x "Metal (MO)" -y "ICOHP (eV/M-O)" -o norm_ICOHP_3d_afm */1_afm/energy_norm_ICOHP.tsv
--- .ipynb_checkpoints/tsv-checkpoint.py
+++ .ipynb_checkpoints/tsv-checkpoint.py
@@ -23,19 +23,19 @@ def plot_patterns_from_multiple_tsv(filenames, output, xlabel, ylabel, labels, a
         indice = [f'{a}\n{b}\n{c}' for a, b, c in zip(metal_rows['3d'], metal_rows['4d'], metal_rows['5d'])]
         if '1_Tetrahedral_WZ' in os.getcwd():
             markers = ['v']
-            colors = plt.cm.Reds(np.linspace(0, 1, len(indice)))
+            colors = plt.cm.Reds(np.linspace(0, 1, len(filenames)))
         elif '2_Tetrahedral_ZB' in os.getcwd():
             markers = ['v']
-            colors = plt.cm.Oranges(np.linspace(0, 1, len(indice)))
+            colors = plt.cm.Oranges(np.linspace(0, 1, len(filenames)))
         elif '3_Square_Planar_TN' in os.getcwd():
             markers = ['s']
-            colors = plt.cm.Greens(np.linspace(0, 1, len(indice)))
+            colors = plt.cm.Greens(np.linspace(0, 1, len(filenames)))
         elif '4_Square_Planar_33' in os.getcwd():
             markers = ['s']
-            colors = plt.cm.Blues(np.linspace(0, 1, len(indice)))
+            colors = plt.cm.Blues(np.linspace(0, 1, len(filenames)))
         elif '5_Octahedral_RS' in os.getcwd():
             markers = ['o']
-            colors = plt.cm.Purples(np.linspace(0, 1, len(indice)))
+            colors = plt.cm.Purples(np.linspace(0, 1, len(filenames)))
         else:
             markers = ['x']
             colors = ['k']
