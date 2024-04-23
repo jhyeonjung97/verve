e8731c4e28d62660dd393429383da981e19169f4
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-04-22 18:48:31 -0700

    .

--- .ipynb_checkpoints/tmp-checkpoint.sh
+++ .ipynb_checkpoints/tmp-checkpoint.sh
@@ -1,2 +1,2 @@
-sh ~/bin/verve/gather.sh -r -c 2 traj $1
-python ~/bin/verve/slab.py -wcsf -r 2,2,2 -a 20 -t traj $1
+sh ~/bin/verve/gather.sh -r -c 2 restart.json $1
+python ~/bin/verve/slab.py -wcsf -r 2,2,2 -a 20 -t json $1
