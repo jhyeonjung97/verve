22ced8dbce23e162ab6aa30b52080320072e0f3b
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-04-29 18:11:35 -0700

    .

--- .ipynb_checkpoints/sum-checkpoint.py
+++ .ipynb_checkpoints/sum-checkpoint.py
@@ -18,6 +18,7 @@ def process_files(add_files, subtract_files, output,
         }   
 
     summed_df = None
+    # summed_index = None
     
     png_filename = f"summed_{output}.png"   
     tsv_filename = f"summed_{output}.tsv"
@@ -27,20 +28,20 @@ def process_files(add_files, subtract_files, output,
         df = pd.read_csv(filename, delimiter='\t')
         if summed_df is None:
             summed_df = df
-            summed_index = df.index
+            # summed_index = df.index
         else:
-            summed_df += df  # Add values excluding the first column
+            summed_df.iloc[:, 1:] += df.iloc[:, 1:]  # Add values excluding the first column
 
     # Process subtraction files
     for filename in subtract_files:
         df = pd.read_csv(filename, delimiter='\t')
         if summed_df is None:
             summed_df = -df  # Subtract values for initialization, excluding the first column
-            summed_index = df.index
+            # summed_index = df.index
         else:
-            summed_df -= df  # Subtract values excluding the first column
+            summed_df.iloc[:, 1:] -= df.iloc[:, 1:]  # Subtract values excluding the first column
     
-    summed_df.index = summed_index
+    # summed_df.index = summed_index
 
     if row:
         indice = metal_rows[row]
