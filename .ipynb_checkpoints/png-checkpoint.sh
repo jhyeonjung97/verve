b1926410e768b6dabdd5e58b552658b01fd5a673
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-05-13 17:25:34 -0700

    .

--- .ipynb_checkpoints/png-checkpoint.sh
+++ .ipynb_checkpoints/png-checkpoint.sh
@@ -179,8 +179,9 @@ elif [[ ${here} == 'nersc' ]]; then
     # python ~/bin/verve/concat.py -o IE3 --X */merged_IE3.tsv
     # python ~/bin/verve/concat.py -o sub --X */merged_sub.tsv
     # python ~/bin/verve/concat.py -o coord --X */merged_coord.tsv
-    python ~/bin/verve/lr.py -o 3 --C concat_coord.tsv -i ICOHP MadelungL CFSE --Y concat_norm_formation.tsv --X concat_ICOHP.tsv concat_norm_MadelungL.tsv concat_cfse.tsv > regression3.log
-    python ~/bin/verve/lr.py -o 4 --C concat_coord.tsv -i MadelungL CFSE IE1 IE2 --Y concat_norm_formation.tsv --X concat_norm_MadelungL.tsv concat_cfse.tsv concat_IE1.tsv concat_IE2.tsv > regression4.log
+    # python ~/bin/verve/lr.py -o 3 --C concat_coord.tsv -i ICOHP MadelungL CFSE --Y concat_norm_formation.tsv --X concat_ICOHP.tsv concat_norm_MadelungL.tsv concat_cfse.tsv > regression3.log
+    python ~/bin/verve/lr.py -o 3 --C concat_coord.tsv -i ICOHP  IE1 IE2 --Y concat_norm_formation.tsv --X concat_ICOHP.tsv concat_IE1.tsv concat_IE2.tsv > regression3.log 
+    # python ~/bin/verve/lr.py -o 4 --C concat_coord.tsv -i MadelungL CFSE IE1 IE2 --Y concat_norm_formation.tsv --X concat_norm_MadelungL.tsv concat_cfse.tsv concat_IE1.tsv concat_IE2.tsv > regression4.log
     python ~/bin/verve/lr.py -o 4 --C concat_coord.tsv -i ICOHP MadelungL IE1 IE2 --Y concat_norm_formation.tsv --X concat_ICOHP.tsv concat_norm_MadelungL.tsv concat_IE1.tsv concat_IE2.tsv > regression4.log 
     python ~/bin/verve/lr.py -o 5 --C concat_coord.tsv -i ICOHP MadelungL IE1 IE2 IE3 --Y concat_norm_formation.tsv --X concat_ICOHP.tsv concat_norm_MadelungL.tsv concat_IE1.tsv concat_IE2.tsv concat_IE3.tsv > regression5.log 
     python ~/bin/verve/lr.py -o 7 --C concat_coord.tsv -i ICOHP MadelungL CFSE IE1 IE2 IE3 E_sub --Y concat_norm_formation.tsv --X concat_ICOHP.tsv concat_norm_MadelungL.tsv concat_cfse.tsv concat_IE1.tsv concat_IE2.tsv concat_IE3.tsv concat_sub.tsv > regression7.log   
