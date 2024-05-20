7d942686376f145fa0fbbc8117f4e0d97a0f4ee4
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-05-20 15:13:12 -0700

    .

--- kj2ev.py
+++ kj2ev.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 X = pd.read_csv('energy_sub.tsv', delimiter='\t')
-Y = pd.read_csv('energy_sub.tsv', delimiter='\t').iloc[:, :1]
+Y = pd.read_csv('energy_norm_formation.tsv', delimiter='\t')[0]
 print(Y)
 # df = df / 96.48
 X.to_csv('energy_sub_eV.tsv', sep='\t', index=False)
