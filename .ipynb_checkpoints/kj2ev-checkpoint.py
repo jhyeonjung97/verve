f0477cc01b449cb50f3f7b401b025dad01cf76b7
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-05-20 15:14:26 -0700

    .

--- .ipynb_checkpoints/kj2ev-checkpoint.py
+++ .ipynb_checkpoints/kj2ev-checkpoint.py
@@ -5,10 +5,13 @@ import pandas as pd
 X = pd.read_csv('energy_sub.tsv', delimiter='\t')
 Y = pd.read_csv('energy_norm_formation.tsv', delimiter='\t', header=None, index_col=0)
 
+
+print(X)
+
+print(Y)
+
 # Set the index of X to match the index of Y
 X.index = Y.index
 
 # Save the modified X to a new file
 X.to_csv('energy_sub_eV.tsv', sep='\t', index=True)
-
-print(X)
