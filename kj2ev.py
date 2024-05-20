e7abe994125f78dc35c959a8f91ca796319903ed
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-05-20 15:03:18 -0700

    .

--- kj2ev.py
+++ kj2ev.py
@@ -1,15 +1,6 @@
-import argparse
 import numpy as np
 import pandas as pd
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--X', required=True, help='Path to the input CSV file')
-    args = parser.parse_args()
-
-    df = pd.read_csv(args.X, delimiter='\t').iloc[:, 1:]
-    df = df / 96.48
-    df.to_csv('test.tsv', sep='\t', index=False)
-
-if __name__ == "__main__":
-    main()
+df = pd.read_csv('energy_sub.tsv', delimiter='\t').iloc[:, 1:]
+df = df / 96.48
+df.to_csv('energy_sub_eV.tsv', sep='\t', index=False)
