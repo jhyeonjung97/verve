5760fce35cc8321774cd423e356abe12352520d4
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-05-20 15:12:35 -0700

    .

--- kj2ev.py
+++ kj2ev.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('energy_sub.tsv', delimiter='\t').iloc[:, 1:]
-df = df / 96.48
-df.to_csv('energy_sub_eV.tsv', sep='\t', index=False)
+X = pd.read_csv('energy_sub.tsv', delimiter='\t')
+Y = pd.read_csv('energy_sub.tsv', delimiter='\t').iloc[:, :1]
+print(Y)
+# df = df / 96.48
+X.to_csv('energy_sub_eV.tsv', sep='\t', index=False)
