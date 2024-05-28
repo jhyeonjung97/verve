75e24656c3f92cb619cda0504d8feb50d03afa5e
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-05-28 00:38:31 -0700

    .

--- .ipynb_checkpoints/add-checkpoint.py
+++ .ipynb_checkpoints/add-checkpoint.py
@@ -1,17 +1,18 @@
 from ase.io import read, write
+from ase import Atoms
 from ase.atom import Atom
 
 # Read the initial atomic structure from 'start.traj'
 atoms = read('start.traj')
 
-# Create a new list excluding O and H atoms
-atoms = [atom for atom in atoms if atom.symbol not in ['O', 'H']]
+# Create a new Atoms object excluding O and H atoms
+filtered_atoms = Atoms([atom for atom in atoms if atom.symbol not in ['O', 'H']])
 
 # List to hold new atoms
 new_atoms = []
 
 # Add new atoms around Mn atoms
-for atom in atoms:
+for atom in filtered_atoms:
     if atom.symbol == 'Mn':
         new_atoms.append(Atom('O', position=atom.position + (0.0, 0.0, +1.8)))
         new_atoms.append(Atom('O', position=atom.position + (0.0, 0.0, -1.8)))
@@ -19,8 +20,8 @@ for atom in atoms:
         # Uncomment the following line if you want to add this hydrogen atom as well
         # new_atoms.append(Atom('H', position=atom.position + (0.0, 0.0, -1.8) + (-0.8, 0.0, -0.6)))
 
-# Extend the atoms list with new atoms
-atoms.extend(new_atoms)
+# Extend the filtered_atoms list with new atoms
+filtered_atoms.extend(new_atoms)
 
 # Write the modified structure to 'start.traj'
-write('hello.traj', atoms)
+write('hello.traj', filtered_atoms)
