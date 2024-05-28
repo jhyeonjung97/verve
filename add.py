073bede98a550bfec1a8c0aa93711477c7996c37
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-05-28 00:36:18 -0700

    .

--- add.py
+++ add.py
@@ -7,13 +7,20 @@ atoms = read('start.traj')
 # Create a new list excluding O and H atoms
 atoms = [atom for atom in atoms if atom.symbol not in ['O', 'H']]
 
+# List to hold new atoms
+new_atoms = []
+
 # Add new atoms around Mn atoms
 for atom in atoms:
     if atom.symbol == 'Mn':
-        atoms.append(Atom('O', position=atom.position + (0.0, 0.0, +1.8)))
-        atoms.append(Atom('O', position=atom.position + (0.0, 0.0, -1.8)))
-        atoms.append(Atom('H', position=atom.position + (0.0, 0.0, +1.8) + (+0.8, 0.0, +0.6)))
-        # atoms.append(Atom('H', position=atom.position + (0.0, 0.0, -1.8) + (-0.8, 0.0, -0.6)))
+        new_atoms.append(Atom('O', position=atom.position + (0.0, 0.0, +1.8)))
+        new_atoms.append(Atom('O', position=atom.position + (0.0, 0.0, -1.8)))
+        new_atoms.append(Atom('H', position=atom.position + (0.0, 0.0, +1.8) + (+0.8, 0.0, +0.6)))
+        # Uncomment the following line if you want to add this hydrogen atom as well
+        # new_atoms.append(Atom('H', position=atom.position + (0.0, 0.0, -1.8) + (-0.8, 0.0, -0.6)))
+
+# Extend the atoms list with new atoms
+atoms.extend(new_atoms)
 
 # Write the modified structure to 'start.traj'
-write('start.traj', atoms)
+write('hello.traj', atoms)
