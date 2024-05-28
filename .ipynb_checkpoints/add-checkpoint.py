b5aa8d6be10bb808146fcde1e2578d02403c4d1a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-05-28 00:39:39 -0700

    .

--- .ipynb_checkpoints/add-checkpoint.py
+++ .ipynb_checkpoints/add-checkpoint.py
@@ -20,8 +20,8 @@ for atom in filtered_atoms:
         # Uncomment the following line if you want to add this hydrogen atom as well
         # new_atoms.append(Atom('H', position=atom.position + (0.0, 0.0, -1.8) + (-0.8, 0.0, -0.6)))
 
-# Extend the filtered_atoms list with new atoms
-filtered_atoms.extend(new_atoms)
+# Combine the filtered_atoms and new_atoms into one Atoms object
+combined_atoms = filtered_atoms + Atoms(new_atoms)
 
 # Write the modified structure to 'start.traj'
-write('hello.traj', filtered_atoms)
+write('hello.traj', combined_atoms)
