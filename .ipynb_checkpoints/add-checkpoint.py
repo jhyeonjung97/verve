422b1a9fdf55825e3ee7da33d19b82fe1225dcc6
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-05-28 00:45:57 -0700

    .

--- .ipynb_checkpoints/add-checkpoint.py
+++ .ipynb_checkpoints/add-checkpoint.py
@@ -3,14 +3,14 @@ from ase import Atoms
 from ase.atom import Atom
 
 atoms = read('start.traj')
-filtered_atoms = [atom for atom in atoms if atom.symbol not in ['O', 'H']]
+del atoms[[atom.symbol == 'Li' for atom in atoms]]
 
 for atom in filtered_atoms:
     if atom.symbol == 'Mn':
-        filtered_atoms.append(Atom('O', position=atom.position + (0.0, 0.0, +1.8)))
-        filtered_atoms.append(Atom('O', position=atom.position + (0.0, 0.0, -1.8)))
-        filtered_atoms.append(Atom('H', position=atom.position + (0.0, 0.0, +1.8) + (+0.8, 0.0, +0.6)))
+        atoms.append(Atom('O', position=atom.position + (0.0, 0.0, +1.8)))
+        atoms.append(Atom('O', position=atom.position + (0.0, 0.0, -1.8)))
+        atoms.append(Atom('H', position=atom.position + (0.0, 0.0, +1.8) + (+0.8, 0.0, +0.6)))
         break
         # atoms.append(Atom('H', position=atom.position + (0.0, 0.0, -1.8) + (-0.8, 0.0, -0.6)))
 
-write('hello.traj', filtered_atoms)
+write('hello.traj', atoms)
