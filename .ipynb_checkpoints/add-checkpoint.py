9dc9e1d3fedf871edfa7a4f427ee71176b8160fd
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-05-28 00:31:28 -0700

    .

--- .ipynb_checkpoints/add-checkpoint.py
+++ .ipynb_checkpoints/add-checkpoint.py
@@ -1,14 +1,24 @@
 from ase.io import read, write
 from ase.atom import Atom
 
+# Read the initial atomic structure from 'start.traj'
 atoms = read('start.traj')
-for atom in atoms:
-    if atom.symbol == 'O' or atom.symbol == 'H':
-        del atom
+
+# Create a new list excluding O and H atoms
+atoms = [atom for atom in atoms if atom.symbol not in ['O', 'H']]
+
+# Add new atoms around Mn atoms
+new_atoms = []
 for atom in atoms:
     if atom.symbol == 'Mn':
-        atoms.append(Atom('O', position=atom.position+(0.0,0.0,+1.8)))
-        atoms.append(Atom('O', position=atom.position+(0.0,0.0,-1.8)))
-        atoms.append(Atom('H', position=atom.position+(0.0,0.0,+1.8)+(+0.8,0.0,+0.6)))
-        # atoms.append(Atom('H', position=atom.position+(0.0,0.0,-1.8)+(-0.8,0.0,-0.6)))
+        new_atoms.append(Atom('O', position=atom.position + (0.0, 0.0, 1.8)))
+        new_atoms.append(Atom('O', position=atom.position + (0.0, 0.0, -1.8)))
+        new_atoms.append(Atom('H', position=atom.position + (0.0, 0.0, 1.8) + (0.8, 0.0, 0.6)))
+        # Uncomment the following line if you want to add this hydrogen atom as well
+        # new_atoms.append(Atom('H', position=atom.position + (0.0, 0.0, -1.8) + (-0.8, 0.0, -0.6)))
+
+# Add the new atoms to the existing list
+atoms.extend(new_atoms)
+
+# Write the modified structure to 'start.traj'
 write('start.traj', atoms)
