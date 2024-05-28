232ff68f1afe0fc77b5c2f642b6c579ba4feb271
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-05-28 00:10:33 -0700

    .

--- .ipynb_checkpoints/add-checkpoint.py
+++ .ipynb_checkpoints/add-checkpoint.py
@@ -1,7 +1,7 @@
 from ase.io import read, write
 from ase.atom import Atom
 
-atoms = read('start.traj')
+atoms = read('CONTCAR')
 for atom in atoms:
     if atom.symbol == 'O':
         atoms.append(Atom('H', position=atom.position+(0.8,0.0,0.6)))
