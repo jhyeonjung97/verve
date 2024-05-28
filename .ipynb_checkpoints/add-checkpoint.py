58ad4af4b3a6ac5792fc855d4643c8183a5cd3b9
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-05-28 00:42:59 -0700

    .

--- .ipynb_checkpoints/add-checkpoint.py
+++ .ipynb_checkpoints/add-checkpoint.py
@@ -3,7 +3,12 @@ from ase import Atoms
 from ase.atom import Atom
 
 atoms = read('start.traj')
-del atoms[[atom.index for atom in atoms if atom.symbol=='O' or atom.symbol=='H']]
+
+for atom in atoms:
+    if atom.symbol=='O' or atom.symbol=='H': 
+        del atom
+
+print(atoms)
 
 for atom in atoms:
     if atom.symbol == 'Mn':
@@ -13,4 +18,4 @@ for atom in atoms:
         break
         # atoms.append(Atom('H', position=atom.position + (0.0, 0.0, -1.8) + (-0.8, 0.0, -0.6)))
 
-write('hello.traj', combined_atoms)
+write('hello.traj', atoms)
