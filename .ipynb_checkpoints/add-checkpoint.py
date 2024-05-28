97f24ac115257e450ab27a0f34f93524135890ad
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-05-28 00:41:54 -0700

    .

--- .ipynb_checkpoints/add-checkpoint.py
+++ .ipynb_checkpoints/add-checkpoint.py
@@ -2,26 +2,15 @@ from ase.io import read, write
 from ase import Atoms
 from ase.atom import Atom
 
-# Read the initial atomic structure from 'start.traj'
 atoms = read('start.traj')
+del atoms[[atom.index for atom in atoms if atom.symbol=='O' or atom.symbol=='H']]
 
-# Create a new Atoms object excluding O and H atoms
-filtered_atoms = Atoms([atom for atom in atoms if atom.symbol not in ['O', 'H']])
-
-# List to hold new atoms
-new_atoms = []
-
-# Add new atoms around Mn atoms
-for atom in filtered_atoms:
+for atom in atoms:
     if atom.symbol == 'Mn':
-        new_atoms.append(Atom('O', position=atom.position + (0.0, 0.0, +1.8)))
-        new_atoms.append(Atom('O', position=atom.position + (0.0, 0.0, -1.8)))
-        new_atoms.append(Atom('H', position=atom.position + (0.0, 0.0, +1.8) + (+0.8, 0.0, +0.6)))
-        # Uncomment the following line if you want to add this hydrogen atom as well
-        # new_atoms.append(Atom('H', position=atom.position + (0.0, 0.0, -1.8) + (-0.8, 0.0, -0.6)))
+        atoms.append(Atom('O', position=atom.position + (0.0, 0.0, +1.8)))
+        atoms.append(Atom('O', position=atom.position + (0.0, 0.0, -1.8)))
+        atoms.append(Atom('H', position=atom.position + (0.0, 0.0, +1.8) + (+0.8, 0.0, +0.6)))
+        break
+        # atoms.append(Atom('H', position=atom.position + (0.0, 0.0, -1.8) + (-0.8, 0.0, -0.6)))
 
-# Combine the filtered_atoms and new_atoms into one Atoms object
-combined_atoms = filtered_atoms + Atoms(new_atoms)
-
-# Write the modified structure to 'start.traj'
 write('hello.traj', combined_atoms)
