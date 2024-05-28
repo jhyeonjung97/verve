35744b72829b4aa2c92fb7b5a7b763b062a6bfa6
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-05-28 01:02:11 -0700

    .

--- .ipynb_checkpoints/add-checkpoint.py
+++ .ipynb_checkpoints/add-checkpoint.py
@@ -7,8 +7,8 @@ atoms = read('start.traj')
 for atom in atoms:
     if atom.symbol == 'Mn':
         p = atom.position
-    elif atom.symbol == 'O' and atom.position[2] > p[2]:
-        atoms.append(Atom('H', position=atom.position + (+0.8, 0.0, +0.6)))
-        # atoms.append(Atom('H', position=atom.position + (-0.8, 0.0, -0.6)))
+    elif atom.symbol == 'O' and atom.position[2] < p[2]:
+        # atoms.append(Atom('H', position=atom.position + (+0.8, 0.0, +0.6)))
+        atoms.append(Atom('H', position=atom.position + (-0.8, 0.0, -0.6)))
 
 write('start.traj', atoms)
