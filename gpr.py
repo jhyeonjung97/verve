b611b2ee8676ebc71acf864acb32607e1c79e8c8
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-05-29 15:38:43 -0700

    .

--- gpr.py
+++ gpr.py
@@ -76,89 +76,88 @@ def main():
     X = df_combined.iloc[:, -(len(args.index) + 1):-1]
     Y = df_combined['E_form']
 
-    # Model fitting and evaluation
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3)
+#     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3)
 
-    params = [{'alpha': np.logspace(-3, 2, 200)}]
-    model = GridSearchCV(GPR(normalize_y=True), params, cv=5)
+#     params = [{'alpha': np.logspace(-3, 2, 200)}]
+#     model = GridSearchCV(GPR(normalize_y=True), params, cv=5)
     
-    poly = PolynomialFeatures(degree=2)
-    pipe = Pipeline([
-        ('scaler', StandardScaler()),
-        ('model', model),
-    ])
+#     poly = PolynomialFeatures(degree=2)
+#     pipe = Pipeline([
+#         ('scaler', StandardScaler()),
+#         ('model', model),
+#     ])
     
-    score = cross_validate(pipe, X_train, y_train, scoring=['r2', 'neg_mean_absolute_error'], cv=5)
-    print(score)
-    print('CV scores:')
-    print(np.mean(score['test_neg_mean_absolute_error']))
-    print(np.mean(score['test_r2']))
+#     score = cross_validate(pipe, X_train, y_train, scoring=['r2', 'neg_mean_absolute_error'], cv=5)
+#     print(score)
+#     print('CV scores:')
+#     print(np.mean(score['test_neg_mean_absolute_error']))
+#     print(np.mean(score['test_r2']))
     
-    pipe.fit(X_train, y_train)
-    opt_alpha = float(pipe['model'].best_estimator_.get_params()['alpha'])
+#     pipe.fit(X_train, y_train)
+#     opt_alpha = float(pipe['model'].best_estimator_.get_params()['alpha'])
 
-    model = GPR(normalize_y=True, alpha=opt_alpha)
-    model.fit(X, Y)
+#     model = GPR(normalize_y=True, alpha=opt_alpha)
+#     model.fit(X, Y)
     
-    Y_pred = model.predict(X)
-    mae = mean_absolute_error(Y, Y_pred)
-    mse = mean_squared_error(Y, Y_pred)
+#     Y_pred = model.predict(X)
+#     mae = mean_absolute_error(Y, Y_pred)
+#     mse = mean_squared_error(Y, Y_pred)
 
-    # Save results
-    tsv_filename = f'regression{filename}.tsv'
-    png_filename = f'regression{filename}.png'
-    log_filename = f'regression{filename}.log'
+#     # Save results
+#     tsv_filename = f'regression{filename}.tsv'
+#     png_filename = f'regression{filename}.png'
+#     log_filename = f'regression{filename}.log'
     
-    df_combined.to_csv(tsv_filename, sep='\t', index=False)
+#     df_combined.to_csv(tsv_filename, sep='\t', index=False)
 
-    with open(log_filename, 'w') as file:
-        file.write(f"\nR-squared: {model.score(X, Y)}\n")
-        file.write(f"Mean Absolute Error: {mae}\n")
-        file.write(f"Mean Squared Error: {mse}\n\n")
+#     with open(log_filename, 'w') as file:
+#         file.write(f"\nR-squared: {model.score(X, Y)}\n")
+#         file.write(f"Mean Absolute Error: {mae}\n")
+#         file.write(f"Mean Squared Error: {mse}\n\n")
     
-    df_combined['Predicted E_form'] = Y_pred
-    df_combined['Residuals'] = Y - Y_pred
+#     df_combined['Predicted E_form'] = Y_pred
+#     df_combined['Residuals'] = Y - Y_pred
 
-    # Plot results
-    plt.figure(figsize=(10, 8))
-    colors = ['red', 'green', 'blue']
-    markers = ['v', '^', 's', 'D', 'o']
-    for i, row in enumerate([3, 4, 5]):
-        sub = df_combined[df_combined['Row'] == row]
-        for j, coordination in enumerate(['WZ', 'ZB', 'TN', '33', 'RS']):
-            subset = sub[sub['Coordination'] == coordination]
-            plt.scatter(subset['E_form'], subset['Predicted E_form'], alpha=0.3, color=colors[i], marker=markers[j], label=f'{row}_{coordination}')
-            for (x, y, label) in zip(subset['E_form'], subset['Predicted E_form'], subset['Metal']):
-                plt.annotate(label, (x, y))
+#     # Plot results
+#     plt.figure(figsize=(10, 8))
+#     colors = ['red', 'green', 'blue']
+#     markers = ['v', '^', 's', 'D', 'o']
+#     for i, row in enumerate([3, 4, 5]):
+#         sub = df_combined[df_combined['Row'] == row]
+#         for j, coordination in enumerate(['WZ', 'ZB', 'TN', '33', 'RS']):
+#             subset = sub[sub['Coordination'] == coordination]
+#             plt.scatter(subset['E_form'], subset['Predicted E_form'], alpha=0.3, color=colors[i], marker=markers[j], label=f'{row}_{coordination}')
+#             for (x, y, label) in zip(subset['E_form'], subset['Predicted E_form'], subset['Metal']):
+#                 plt.annotate(label, (x, y))
     
-    plt.plot([Y.min(), Y.max()], [Y.min(), Y.max()], 'r--', lw=1)
-    plt.xlabel('DFT-calculated Formation Energy (eV)')
-    plt.ylabel('Predicted Formation Energy (eV)')
-    plt.legend()
-    plt.tight_layout()
-    plt.gcf().savefig(png_filename, bbox_inches="tight")
-    plt.close()
+#     plt.plot([Y.min(), Y.max()], [Y.min(), Y.max()], 'r--', lw=1)
+#     plt.xlabel('DFT-calculated Formation Energy (eV)')
+#     plt.ylabel('Predicted Formation Energy (eV)')
+#     plt.legend()
+#     plt.tight_layout()
+#     plt.gcf().savefig(png_filename, bbox_inches="tight")
+#     plt.close()
 
-    # Plot correlation matrix
-    M = pd.concat([Y, X], axis=1)
-    correlation_matrix = M.corr()
-    abs_correlation_matrix = correlation_matrix.abs()
+#     # Plot correlation matrix
+#     M = pd.concat([Y, X], axis=1)
+#     correlation_matrix = M.corr()
+#     abs_correlation_matrix = correlation_matrix.abs()
     
-    plt.figure(figsize=(7, 6))
-    sns.heatmap(correlation_matrix, annot=True, fmt=".2f", cmap='coolwarm')
-    plt.xticks(np.arange(M.shape[1]) + 0.5, M.columns, rotation=90, ha='right')
-    plt.yticks(np.arange(M.shape[1]) + 0.5, M.columns, rotation=0, va='center')
-    plt.tight_layout()
-    plt.gcf().savefig(f'covariance_matrix{filename}.png', bbox_inches="tight")
-    plt.close()
+#     plt.figure(figsize=(7, 6))
+#     sns.heatmap(correlation_matrix, annot=True, fmt=".2f", cmap='coolwarm')
+#     plt.xticks(np.arange(M.shape[1]) + 0.5, M.columns, rotation=90, ha='right')
+#     plt.yticks(np.arange(M.shape[1]) + 0.5, M.columns, rotation=0, va='center')
+#     plt.tight_layout()
+#     plt.gcf().savefig(f'covariance_matrix{filename}.png', bbox_inches="tight")
+#     plt.close()
     
-    plt.figure(figsize=(7, 6))
-    sns.heatmap(abs_correlation_matrix, annot=True, fmt=".2f", cmap='coolwarm', center=0, vmin=0, vmax=1)
-    plt.xticks(np.arange(M.shape[1]) + 0.5, M.columns, rotation=90, ha='right')
-    plt.yticks(np.arange(M.shape[1]) + 0.5, M.columns, rotation=0, va='center')
-    plt.tight_layout()
-    plt.gcf().savefig(f'abs_covariance_matrix{filename}.png', bbox_inches="tight")
-    plt.close()
+#     plt.figure(figsize=(7, 6))
+#     sns.heatmap(abs_correlation_matrix, annot=True, fmt=".2f", cmap='coolwarm', center=0, vmin=0, vmax=1)
+#     plt.xticks(np.arange(M.shape[1]) + 0.5, M.columns, rotation=90, ha='right')
+#     plt.yticks(np.arange(M.shape[1]) + 0.5, M.columns, rotation=0, va='center')
+#     plt.tight_layout()
+#     plt.gcf().savefig(f'abs_covariance_matrix{filename}.png', bbox_inches="tight")
+#     plt.close()
 
 if __name__ == "__main__":
     main()
