8492b235764e244b482e42fa29563411f4c59e81
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-05-31 17:43:17 -0700

    .

--- .ipynb_checkpoints/nn-hyperopt-checkpoint.py
+++ .ipynb_checkpoints/nn-hyperopt-checkpoint.py
@@ -156,10 +156,10 @@ def main():
             model__dropout2=params['dropout2'], 
             model__units3=params['units3'], 
             model__dropout3=params['dropout3'], 
-            model__activation=['relu', 'tanh', 'sigmoid'][params['activation']],
+            model__activation=params['activation'],
             model__last_linear=params['last_linear'],
             model__learning_rate=params['learning_rate'],
-            model__optimizer=['Adam', 'SGD', 'RMSprop'][params['optimizer']],
+            model__optimizer=params['optimizer'],
             epochs=params['epochs'],
             batch_size=params['batch_size'],
             verbose=0
