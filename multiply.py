6694f68ae85ed2a9824c6c9ee735180b57033eb9
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-06-10 10:33:45 -0700

    .

--- multiply.py
+++ multiply.py
@@ -2,14 +2,14 @@ import numpy as np
 import pandas as pd
 
 # Load the data from CSV files
-X = pd.read_csv('merged_norm_MadelungL.tsv', delimiter='\t', index_col=0)
+X = pd.read_csv('merged_norm_ICOHP.tsv', delimiter='\t', index_col=0)
 Y = pd.read_csv('merged_ICOBI.tsv', delimiter='\t', index_col=0)
 
 # Perform element-wise division
-Z = X / Y
+Z = X * Y
 
 # Ensure the index of Z matches the index of X
 Z.index = X.index
 
 # Save the result to a new TSV file
-Z.to_csv('merged_weighted_norm_MadelungL.tsv', sep='\t', index=True)
+Z.to_csv('merged_weighted_ICOHP.tsv', sep='\t', index=True)
