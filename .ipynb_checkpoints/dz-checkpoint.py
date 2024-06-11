cb8afb5c1258fc8d1ac49a680614019a1512229c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-06-10 23:02:45 -0700

    .

--- .ipynb_checkpoints/dz-checkpoint.py
+++ .ipynb_checkpoints/dz-checkpoint.py
@@ -5,8 +5,6 @@ from statistics import mean
 atoms = read('CONTCAR')
 
 zN = mean([atom.z for atom in atoms if atom.symbol == 'N'])
-print([atom.z for atom in atoms if atom.symbol == 'N'])
-zM = mean([atom.z for atom in atoms if atom.symbol != 'N' and atom.symbol != 'O' and atom.symbol != 'H'])
-print([atom.z for atom in atoms if atom.symbol != 'N' and atom.symbol != 'O' and atom.symbol != 'H'])
+zM = mean([atom.z for atom in atoms if atom.symbol != 'N' and atom.symbol != 'C' and atom.symbol != 'O' and atom.symbol != 'H'])
 dz = abs(zN - zM)
 print(dz)
