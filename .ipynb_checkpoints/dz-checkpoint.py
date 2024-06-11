1ccbb819c5bdad73a51c127dfae9131f6ff1cdac
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-06-10 23:01:16 -0700

    .

--- .ipynb_checkpoints/dz-checkpoint.py
+++ .ipynb_checkpoints/dz-checkpoint.py
@@ -6,5 +6,7 @@ atoms = read('CONTCAR')
 
 zN = mean([atom.z for atom in atoms if atom.symbol == 'N'])
 zM = mean([atom.z for atom in atoms if atom.symbol != 'N' and atom.symbol != 'O' and atom.symbol != 'H'])
+print(zN)
+print(zM)
 dz = abs(zN - zM)*20
 print(dz)
