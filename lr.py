aad3af8d710a92780af7c40f8fabfeb9a69168ee
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-06-11 10:18:14 -0700

    .

--- lr.py
+++ lr.py
@@ -139,7 +139,7 @@ def main():
     abs_correlation_matrix = correlation_matrix.abs()
     
     plt.figure(figsize=(10, 8), dpi=100) # Set the figure size as needed
-    sns.heatmap(correlation_matrix, annot=True, fmt=".2f", annot_kws={"size": 7}, cmap='coolwarm')
+    sns.heatmap(correlation_matrix, annot=True, fmt=".2f", annot_kws={"size": 8}, cmap='coolwarm')
     plt.xticks(np.arange(M.shape[1]) + 0.5, M.columns, rotation=90, ha='right')
     plt.yticks(np.arange(M.shape[1]) + 0.5, M.columns, rotation=0, va='center')
     plt.tight_layout()
