2cc246c4ac0027cf148e98a579f0c51c7723cbac
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-06-10 23:02:14 -0700

    .

--- dz.py
+++ dz.py
@@ -5,8 +5,8 @@ from statistics import mean
 atoms = read('CONTCAR')
 
 zN = mean([atom.z for atom in atoms if atom.symbol == 'N'])
+print([atom.z for atom in atoms if atom.symbol == 'N'])
 zM = mean([atom.z for atom in atoms if atom.symbol != 'N' and atom.symbol != 'O' and atom.symbol != 'H'])
-print(zN)
-print(zM)
-dz = abs(zN - zM)*20
+print([atom.z for atom in atoms if atom.symbol != 'N' and atom.symbol != 'O' and atom.symbol != 'H'])
+dz = abs(zN - zM)
 print(dz)
