12a6f43bfff29a274550267d65be26b56281b466
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-06-17 13:02:51 -0700

    .

--- .ipynb_checkpoints/slab-checkpoint.py
+++ .ipynb_checkpoints/slab-checkpoint.py
@@ -70,6 +70,7 @@ for file in matching_files:
         min_z = atoms.positions[:,2].min()
         max_z = atoms.positions[:,2].max()
         mid_z = (min_z + max_z) / 3
+        print(mid_z)
         fixed = FixAtoms(indices=[atom.index for atom in atoms if atom.position[2] < mid_z])
         atoms.set_constraint(fixed)
     if args.center:
