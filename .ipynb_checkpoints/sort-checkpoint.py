edb154892cdec894c6207495c4a91834511b030a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-06-25 18:58:59 -0700

    .

--- .ipynb_checkpoints/sort-checkpoint.py
+++ .ipynb_checkpoints/sort-checkpoint.py
@@ -12,12 +12,12 @@ def main(input_file, output_file):
     sorted_atoms = metal_atoms + oxygen_atoms
     
     # Create a new Atoms object with the sorted atoms
-    sorted_atoms_obj = atoms.copy()
-    sorted_atoms_obj.set_positions([atom.position for atom in sorted_atoms])
-    sorted_atoms_obj.set_chemical_symbols([atom.symbol for atom in sorted_atoms])
+    # sorted_atoms_obj = atoms.copy()
+    # sorted_atoms_obj.set_positions([atom.position for atom in sorted_atoms])
+    # sorted_atoms_obj.set_chemical_symbols([atom.symbol for atom in sorted_atoms])
     
     # Write the sorted atoms back to a POSCAR file
-    write(output_file, sorted_atoms_obj, format='vasp')
+    write(output_file, sorted_atoms, format='vasp')
     print(f"Sorted POSCAR file has been saved as {output_file}")
 
 if __name__ == "__main__":
