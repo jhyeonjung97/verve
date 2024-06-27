6007be6f7d2e66bb7a20f108aa520d6a809b2572
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-06-27 05:51:24 -0700

    .

--- sumo.sh
+++ sumo.sh
@@ -12,8 +12,8 @@ sumo-dosplot \
     --format png \
     --dpi 100 \
     --column 1 \
-    --width 8 \
-    --height 6 \
+    --width 6 \
+    --height 4 \
     --yscale 1 \
     --zero-line \
     --prefix $prefix \
