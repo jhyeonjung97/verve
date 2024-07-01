1a0b14a737eba8b0e3d0d466bdc6ec6b476e2a55
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-06-30 20:52:03 -0700

    .

--- .ipynb_checkpoints/add-h-checkpoint.py
+++ .ipynb_checkpoints/add-h-checkpoint.py
@@ -1,7 +1,7 @@
 from ase.io import read, write
 from ase import Atoms
 
-atoms = read('modified_structure.json')
+atoms = read('restart.json')
 atoms += Atoms('H', positions=[atoms[-1].position + (0.8, 0, 0.6)])
 
-write('CONTCAR', atoms)
+write('modified_structure.json', atoms)
