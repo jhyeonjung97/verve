a369eb01260296236871a2fcd695b93e13aa2f1b
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-07-01 17:59:17 -0700

    .

--- surface_energy.py
+++ surface_energy.py
@@ -49,8 +49,8 @@ for i in range(6):
                 else:
                     surface_df.iloc[k, 0] = np.nan
 
-            png_filename = f"surface_{i}{coord}_{row_key}.png"
-            tsv_filename = f"surface_{i}{coord}_{row_key}.tsv"
+            png_filename = f"surface_{coord}_{row_key}.png"
+            tsv_filename = f"surface_{coord}_{row_key}.tsv"
 
             plt.figure(figsize=(8, 6))
         
