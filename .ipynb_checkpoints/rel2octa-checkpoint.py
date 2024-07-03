e99565f4b2b0186708b48835ce6c4c0b89c93133
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-07-03 11:44:48 -0700

    .

--- .ipynb_checkpoints/rel2octa-checkpoint.py
+++ .ipynb_checkpoints/rel2octa-checkpoint.py
@@ -3,16 +3,16 @@ import sys
 
 # Example read and write functions (replace these with your actual implementation)
 def read(data):
-    return pd.read_csv(data)
+    return pd.read_csv(data, delimiter='\t', index_col=0)
 
 def write(df, filename):
-    df.to_csv(filename, index=False)
+    df.to_csv(filename, sep='\t')
 
 # Assume data is the first argument
 data = sys.argv[1]
 
-# Read the data
-df = pd.read_csv(data, delimiter='\t').iloc[:, 1:]
+# Read the data, keeping the first column as the index
+df = read(data)
 
 # Split filename and extension correctly
 filename, ext = data.rsplit('.', 1)
@@ -24,11 +24,11 @@ for metal_row in ['3d', '4d', '5d']:
             index1 = i * j
             index2 = 6 * j
             if index1 < len(df) and index2 < len(df):
-                if not pd.isna(df.loc[index1, metal_row]) and not pd.isna(df.loc[index2, metal_row]):
-                    df.loc[index1, metal_row] = df.loc[index1, metal_row] - df.loc[index2, metal_row]
+                if not pd.isna(df.loc[df.index[index1], metal_row]) and not pd.isna(df.loc[df.index[index2], metal_row]):
+                    df.loc[df.index[index1], metal_row] = df.loc[df.index[index1], metal_row] - df.loc[df.index[index2], metal_row]
 
 # Print the DataFrame (optional)
 print(df)
 
-# Write the modified DataFrame to a new file
+# Write the modified DataFrame to a new file, keeping the index
 write(df, filename=f'{filename}_rel.{ext}')
