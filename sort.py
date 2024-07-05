14d46614c1dc710ae5176436d07d9d26a5668ec5
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-07-05 13:52:46 -0700

    .

--- sort.py
+++ sort.py
@@ -32,7 +32,7 @@ def main(input_file, output_file):
     
     # Calculate the displacement to move the first atom to the center of the x and y axes
     displacement = [-first_atom_position[0], -first_atom_position[1], 0]
-    # displacement = [ center_x- first_atom_position[0],center_y -first_atom_position[1], 0]
+    # displacement = [center_x - first_atom_position[0], center_y - first_atom_position[1], 0]
     
     # Apply the displacement to all atoms
     sorted_atoms_obj.translate(displacement)
