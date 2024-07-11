b605227ee10911d83a36aeec4e377fc82727f743
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-07-11 12:20:24 -0700

    .

--- .ipynb_checkpoints/tmp-checkpoint.sh
+++ .ipynb_checkpoints/tmp-checkpoint.sh
@@ -1,3 +1,17 @@
-for i in {0..9}; do
-    mv 0$i "0$i"_
-done
+#!/bin/bash
+
+# Get the current working directory
+current_dir=$(pwd)
+
+# Split the path into components
+IFS='/' read -r -a path_components <<< "$current_dir"
+
+# Extract the required parts and split by '_'
+metal=$(echo "${path_components[-3]}" | cut -d'_' -f2)
+spin=$(echo "${path_components[-2]}" | cut -d'_' -f2)
+dz=$(echo "${path_components[-1]}" | cut -d'_' -f1)
+
+# Print the results
+echo "Metal part: $metal"
+echo "Spin part: $spin"
+echo "DZ part: $dz"
