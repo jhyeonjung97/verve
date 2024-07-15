9b891b7cb5461be8a83e1786846adfdf7cd912db
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-07-15 12:26:38 -0700

    .

--- alias.sh
+++ alias.sh
@@ -93,6 +93,7 @@ alias time='grep sec OUTCAR'
 alias formation='python ~/bin/verve/formation.py'
 alias png='sh ~/bin/verve/png.sh'
 alias gap='python ~/bin/verve/gap.py'
+alias done='python /global/cfs/cdirs/m2997/bin/get_restart3'
 
 #SSH
 alias hi='sh ~/bin/verve/gits.sh orange shoulder verve tools
