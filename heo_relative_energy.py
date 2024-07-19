d6158d9009525c03238eb7df4d6a389cd9de9a80
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-07-19 13:13:42 -0700

    .

--- heo_relative_energy.py
+++ heo_relative_energy.py
@@ -17,16 +17,6 @@ png_filename = 'heo_relative_energy.png'
 tsv_mag_filename = 'heo_magnetic_moments.tsv'
 png_mag_filename = 'heo_magnetic_moments.png'
 
-def histogram(df, xlabel, png_filename):
-    plt.figure(figsize=(12, 8))
-    for column in df.columns:
-        plt.hist(df[column].dropna(), bins=20, alpha=0.5, label=str(column))
-    plt.xlabel(xlabel)
-    plt.ylabel('Frequency')
-    plt.legend(title="Metals")
-    plt.savefig(png_filename)
-    plt.show()
-
 def main():
     for i in range(60):
         path = f'/scratch/x2755a09/4_HEO/{i:02d}_/final_with_calculator.json'
@@ -37,7 +27,7 @@ def main():
         energy = atoms.get_total_energy()
         for j, metal in enumerate(prvs.keys()):
             numb[j] = len([atom for atom in atoms if atom.symbol == metal])
-            magmom = mean([atoms.get_magnetic_moments()[atom.index] for atom in atoms if atom.symbol == metal])
+            magmom = mean([abs(atoms.get_magnetic_moments()[atom.index]) for atom in atoms if atom.symbol == metal])
             df_mag.at[i, metal] = magmom
         relative_energy = energy - sum(numb[j] * prvs[metal] / 8 for j, metal in enumerate(prvs.keys()))
         df.at[i, 'energy'] = relative_energy
@@ -49,8 +39,24 @@ def main():
     print(f"Data saved to {tsv_mag_filename}")
 
     # Plotting the data
-    histogram(df=df, xlabel='Relative energy (eV)', png_filename=png_filename)
-    histogram(df=df_mag, xlabel='Magnetic Moments', png_filename=png_mag_filename)
+    plt.figure(figsize=(12, 8))
+    plt.hist(df['energy'].dropna(), bins=np.arange(-2.0, 0.1, 0.1), alpha=0.5)
+    plt.xlabel('Relative energy (eV)')
+    plt.ylabel('Frequency')
+    plt.legend(title="Metals")
+    plt.savefig(png_filename, bbox_inches="tight")
+    print(f"Figure saved as {png_filename}")
+    plt.show()
+
+    plt.figure(figsize=(12, 8))
+    for column in df_mag.columns:
+        plt.hist(df[column].dropna(), bins=np.arange(0, 6, 1), alpha=0.5, label=str(column))
+    plt.xlabel('Magnetic Moments')
+    plt.ylabel('Frequency')
+    plt.legend(title="Metals")
+    plt.savefig(png_mag_filename, bbox_inches="tight")
+    print(f"Figure saved as {png_filename}")
+    plt.show()
 
 if __name__ == "__main__":
     main()
