45df79fd0441066391910cef471e1ba6ad5de9df
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-07-18 21:45:40 -0700

    .

--- .ipynb_checkpoints/add-o-checkpoint.py
+++ .ipynb_checkpoints/add-o-checkpoint.py
@@ -1,7 +1,7 @@
 from ase.io import read, write
 from ase import Atoms
 
-atoms = read('restart.json')
+atoms = read('moments.json')
 for atom in atoms:
     if atom.symbol not in ['C', 'N', 'O', 'H']:
         atoms += Atoms('O', positions=[atom.position + (0, 0, 2.0)])
