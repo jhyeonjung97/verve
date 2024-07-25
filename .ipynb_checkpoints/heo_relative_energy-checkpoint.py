d7a71a0db2cb6afbd37453d6d74db1432f444095
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-07-25 13:31:47 -0700

    .

--- .ipynb_checkpoints/heo_relative_energy-checkpoint.py
+++ .ipynb_checkpoints/heo_relative_energy-checkpoint.py
@@ -144,33 +144,33 @@ def plotting(pattern, figsize, bins, xlabel, xticks, xmin, xmax, width, filename
     plt.close()
 
 def plotting_adv(df, df_ref, pattern, figsize1, figsize2, bins1, bins2, width1, width2, xlabel, xticks, xmin, xmax, filename):
-    # for i, column in enumerate(df_chg.columns):
-    #     plt.figure(figsize=figsize1)
-    #     plt.hist(df_mag[column].dropna(), bins=bins1, alpha=0.5, color=clrs[i], label=str(column), width=width1)
-    #     plt.axvline(x=df_ref.at[i, pattern], color=clrs[i], linestyle='--')
-    #     plt.xlabel(xlabel)
-    #     plt.ylabel('Frequency')
-    #     plt.xticks(xticks)
-    #     plt.xlim(xmin, xmax)
-    #     plt.legend(title="B sites")
-    #     plt.savefig(f'{filename}_{column}.png', bbox_inches="tight")
-    #     print(f"Figure saved as {filename}_{column}.png")
-    #     plt.close()
-    # plt.figure(figsize=figsize2)
-    for i in range(5):
+    for i, column in enumerate(df_chg.columns):
+        plt.figure(figsize=figsize1)
+        plt.hist(df_mag[column].dropna(), bins=bins1, alpha=0.5, color=clrs[i], label=str(column), width=width1)
         plt.axvline(x=df_ref.at[i, pattern], color=clrs[i], linestyle='--')
-    bins = bins2
-    bin_width = 0.2 / (len(df.columns) + 1)  # Calculate new width for each bar
-    for idx, column in enumerate(df.columns):
-        plt.hist(df[column].dropna(), bins=bins2 + idx * bin_width, alpha=0.5, label=str(column), width=bin_width)
-    plt.xlabel(xlabel)
-    plt.ylabel('Frequency')
-    plt.xticks(xticks)
-    plt.xlim(xmin, xmax)
-    plt.legend(title="B sites")
-    plt.savefig(f'{filename}.png', bbox_inches="tight")
-    print(f"Figure saved as {filename}.png")
-    plt.close()
+        plt.xlabel(xlabel)
+        plt.ylabel('Frequency')
+        plt.xticks(xticks)
+        plt.xlim(xmin, xmax)
+        plt.legend(title="B sites")
+        plt.savefig(f'{filename}_{column}.png', bbox_inches="tight")
+        print(f"Figure saved as {filename}_{column}.png")
+        plt.close()
+    plt.figure(figsize=figsize2)
+    # for i in range(5):
+    #     plt.axvline(x=df_ref.at[i, pattern], color=clrs[i], linestyle='--')
+    # bins = bins2
+    # bin_width = 0.2 / (len(df.columns) + 1)  # Calculate new width for each bar
+    # for idx, column in enumerate(df.columns):
+    #     plt.hist(df[column].dropna(), bins=bins2 + idx * bin_width, alpha=0.5, label=str(column), width=bin_width)
+    # plt.xlabel(xlabel)
+    # plt.ylabel('Frequency')
+    # plt.xticks(xticks)
+    # plt.xlim(xmin, xmax)
+    # plt.legend(title="B sites")
+    # plt.savefig(f'{filename}.png', bbox_inches="tight")
+    # print(f"Figure saved as {filename}.png")
+    # plt.close()
 
 if __name__ == '__main__':
     main()
