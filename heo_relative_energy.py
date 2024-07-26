1caa4cbfe8706c8d7c4b94123e13250268383005
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-07-26 13:41:57 -0700

    .

--- heo_relative_energy.py
+++ heo_relative_energy.py
@@ -119,8 +119,8 @@ def main():
     #              figsize1=(8, 6), bins1=np.arange(0, 6, 0.1), width1=0.09, xticks1=np.arange(0, 6, 1), xmin1=-0.5, xmax1=5.5, 
     #              figsize2=(12, 6), bins2=np.arange(0, 6, 0.2), width2=0.2, xticks2=np.arange(0, 6, 1), xmin2=-0.5, xmax2=5.5)
     plotting_adv(df=df_chg, df_ref=df_ref, pattern='charge', xlabel='Bader charge (e-)', filename=chg_filename,
-                 figsize1=(8, 6), bins1=np.arange(0.0, 2.0, 0.05), width1=0.045, xticks1=np.arange(0.0, 2.0, 0.2), xmin1=-0.1, xmax1=2.1, 
-                 figsize2=(12, 6), bins2=np.arange(0.0, 2.1, 0.05), width2=0.05, xticks2=np.arange(0.0, 2.0, 0.2), xmin2=-0.1, xmax2=2.1)
+                 figsize1=(8, 6), bins1=np.arange(1.0, 2.0, 0.01), width1=0.009, xticks1=np.arange(1.0, 2.1, 0.1), xmin1=0.9, xmax1=2.1, 
+                 figsize2=(12, 6), bins2=np.arange(1.0, 2.1, 0.02), width2=0.02, xticks2=np.arange(1.0, 2.1, 0.1), xmin2=0.9, xmax2=2.1)
     
 def saving(df, filename):
     df.to_csv(filename, sep='\t', float_format='%.2f')
