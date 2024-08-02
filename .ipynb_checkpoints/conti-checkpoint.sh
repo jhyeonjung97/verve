4cf9a671d8c8c7a5ade39fd6a0db8d6ba2c2102c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-02 13:14:34 -0700

    .

--- .ipynb_checkpoints/conti-checkpoint.sh
+++ .ipynb_checkpoints/conti-checkpoint.sh
@@ -2,15 +2,17 @@
 
 i=1
 save="conti_$i"
-while [[ -d "conti_$i" ]]
-do
-    i=$(($i+1))
+while [[ -d "$save" ]]; do
+    i=$((i+1))
     save="conti_$i"
 done
-mkdir $save
-mv * $save
-mv "$save"/*/ .
+mkdir "$save"
+find . -maxdepth 1 -type f -exec mv {} "$save" \;
+
 cp "$save"/restart.json .
-cp "$save"/WAVECAR .
 cp "$save"/submit.sh .
+if [[ -s "$save"/WAVECAR ]]; then
+    cp "$save"/WAVECAR .
+fi
+
 sh ~/bin/verve/sub.sh
