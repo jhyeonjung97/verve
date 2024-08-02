161e7a790f04fc797b01b1c11ad8e764c8c356ec
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-02 13:17:22 -0700

    .

--- .ipynb_checkpoints/untitled-checkpoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp 
-	--include="*/" \
-	--include="*.png" \
-	--exclude="*" jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/2_icohp/1_octahedron/1_high_spin/mag .
