5a91b8e772ed4e65188354358e9eff64bbfda230
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-02 20:26:00 -0700

    .

--- bulk.sh
+++ bulk.sh
@@ -10,6 +10,11 @@ case "$file" in
         ;;
 esac
 
+if [[ ! -f $file ]]; then
+    echo "File $file does not exist."
+    exit 1
+fi
+
 if [[ -z $2 ]]; then
     pattern='X'
 else
@@ -23,14 +28,14 @@ metals_5d=(Ba La Hf Ta W Re Os Ir Pt Au Hg Tl Pb)
 mkdir -p 3d 4d 5d
 
 process_metals() {
-    local -n metals=$1
+    metals=$1[@]
     local subdir=$2
     cd $subdir
-    for i in ${!metals[@]}; do
+    for i in ${!metals}; do
         local formatted_index=$(printf "%02d" $i)
         local dir="${formatted_index}_${metals[i]}"
-        mkdir $dir
-        sed "s/$pattern/${metals[i]}/" ../$file > $dir/$file
+        mkdir $dir || { echo "Failed to create directory $dir"; exit 1; }
+        sed "s/$pattern/${metals[i]}/" ../$file > $dir/$file || { echo "Failed to process $file with ${metals[i]}"; exit 1; }
         echo "sed \"s/$pattern/${metals[i]}/\" ../$file > $dir/$file"
     done
     cd ..
@@ -41,8 +46,8 @@ process_metals metals_4d 4d
 process_metals metals_5d 5d
 rm $file
 
+trap 'sed -i -e "/^[^#]/d" ~/bin/temp.sh' EXIT
 sed -i -e "/^[^#]/s/^/#/" ~/bin/temp.sh
 echo "ase convert -f $file start.traj" >> ~/bin/temp.sh
 echo "rm $file" >> ~/bin/temp.sh
 sh ~/bin/verve/temp.sh -rr
-sed -i -e '/^[^#]/d' ~/bin/temp.sh
