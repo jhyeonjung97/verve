5bcf440c12b89e0dafcb7048e9a5f5edb43c19a5
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-02 20:47:18 -0700

    .

--- .ipynb_checkpoints/bulk-checkpoint.sh
+++ .ipynb_checkpoints/bulk-checkpoint.sh
@@ -1,53 +1,38 @@
 #!/bin/bash
 
-file=$1
-case "$file" in
-    POSCAR|CONTCAR|*.vasp)
-        ;;
-    *)
-        echo 'Are you sure..? Give me POSCAR or CONTCAR or *.vasp'
-        exit 1
-        ;;
-esac
+metals_3d=('Ca' 'Sc' 'Ti' 'V' 'Cr' 'Mn' 'Fe' 'Co' 'Ni' 'Cu' 'Zn' 'Ga' 'Ge')
+metals_4d=('Sr' 'Y' 'Zr' 'Nb' 'Mo' 'Tc' 'Ru' 'Rh' 'Pd' 'Ag' 'Cd' 'In' 'Sn')
+metals_5d=('Ba' 'La' 'Hf' 'Ta' 'W' 'Re' 'Os' 'Ir' 'Pt' 'Au' 'Hg' 'Tl' 'Pb')
 
-if [[ ! -f $file ]]; then
-    echo "File $file does not exist."
-    exit 1
-fi
-
-if [[ -z $2 ]]; then
-    pattern='X'
-else
-    pattern=$2
-fi
-
-metals_3d=(Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge)
-metals_4d=(Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn)
-metals_5d=(Ba La Hf Ta W Re Os Ir Pt Au Hg Tl Pb)
+dir_now=$PWD
 
 mkdir -p 3d 4d 5d
-
-process_metals() {
-    local metals=("$@")
-    local subdir=${metals[-1]}
-    unset metals[-1]
-    cd $subdir
-    for metal in "${metals[@]}"; do
-        local dir="${metal}"
-        mkdir -p $dir || { echo "Failed to create directory $dir"; exit 1; }
-        sed "s/$pattern/$metal/" ../$file > $dir/$file || { echo "Failed to process $file with $metal"; exit 1; }
-        echo "sed \"s/$pattern/$metal/\" ../$file > $dir/$file"
+for dir in 3d 4d 5d; do
+    cd $dir
+    for i in "${!metals_${dir:0:1}d[@]}"; do
+        if [ $i -lt 10 ]; then
+            mkdir -p 0"$i"_"${metals_${dir:0:1}d[$i]}"
+        else
+            mkdir -p "$i"_"${metals_${dir:0:1}d[$i]}"
+        fi
     done
-    cd ..
-}
+    cd $dir_now
+done
 
-process_metals "${metals_3d[@]}" 3d
-process_metals "${metals_4d[@]}" 4d
-process_metals "${metals_5d[@]}" 5d
-rm $file
-
-trap 'sed -i -e "/^[^#]/d" ~/bin/temp.sh' EXIT
-sed -i -e "/^[^#]/s/^/#/" ~/bin/temp.sh
-echo "ase convert -f $file start.traj" >> ~/bin/temp.sh
-echo "rm $file" >> ~/bin/temp.sh
-sh ~/bin/verve/temp.sh -rr
+cp $1 POSCAR
+sed -i -e "s/$2/XX/" POSCAR
+sh ~/bin/verve/spread.sh -rr POSCAR
+for dir in *d/*_*/; do
+    cd $dir
+    metal=$(basename $PWD | cut -d'_' -f2)
+    sed -i -e "s/XX/$metal/" POSCAR
+    # ase convert POSCAR start.traj
+    cd $dir_now
+done
+for dir in *d/; do
+    cd $dir
+    coord=$(basename $PWD | cut -d'_' -f3)
+    clean_dir=$(basename $dir)
+    sh ~/bin/verve/jobname.sh -rc "$coord$clean_dir"
+    cd $dir_now
+done
