25774c657d3a58db73db57c3256d3d83d2960e74
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-08 11:18:19 -0700

    .

--- archieve.sh
+++ archieve.sh
@@ -1,9 +1,9 @@
 dir_now=$PWD
 
 if [[ ${here} == 'slac' ]]; then
-    cd /Users/jiuy97/Google\ Drive/My\ Drive
-elif [[ ${here} == 'mac' ]]; then
-    cd /Users/hailey/jiuy97@stanford.edu\ -\ Google\ Drive/My\ Drive
+    cd /Users/jiuy97/OneDrive\ -\ Stanford
+else
+    exit
 fi
 
 cp -r /Users/jiuy97/bin/orange/* ./bin/orange
@@ -13,5 +13,8 @@ cp -r /Users/jiuy97/bin/verve/* ./bin/verve
 /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/1_cation .
 /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/2_icohp .
 /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/3_V_shape .
-
+/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/4_V_slab .
+/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/5_HEO .
+/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/6_MNC .
+  
 cd $dir_now
