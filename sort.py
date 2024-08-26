38193991fd6e01c5b8941e4ca571251165afa76e
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-26 23:56:05 +0900

    .

--- sort.py
+++ sort.py
@@ -37,7 +37,7 @@ def main(input_file, output_file):
     # Apply the displacement to all atoms
     # sorted_atoms_obj.translate(displacement)
     sorted_atoms_obj.wrap()
-    sorted_atoms_obj.center()
+    # sorted_atoms_obj.center()
 
     # Write the sorted and shifted atoms back to a POSCAR file
     write(output_file, sorted_atoms_obj, format='vasp')
