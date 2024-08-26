40e0f107d72d31d326ee049a88b178394e83690c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-27 00:33:24 +0900

    .

--- tmp.py
+++ tmp.py
@@ -10,19 +10,21 @@ from ase.constraints import FixAtoms
 from ase.geometry.geometry import get_duplicate_atoms
 from ase.io.vasp import read_vasp_xdatcar, write_vasp_xdatcar
 
-pattern = os.path.join('./', '2*.vasp')
+pattern = os.path.join('./', '7*.vasp')
 matching_files = glob.glob(pattern)
 for file in matching_files:
     print(file)
     atoms = read(f'{file}')
     
-    # # displacement = [0, 0, atoms[0].position[2]-0.1]
+    # displacement = [0, 0, atoms[0].position[2]-0.1]
+    cell = atoms.get_cell()
     # displacement = atoms[0].position
-    # atoms.positions -= displacement
-    # # atoms.positions += [0.1, 0.1, 0.1]
-    # atoms.wrap()
-    # # atoms.center()
-    # get_duplicate_atoms(atoms, cutoff=0.1, delete=True)
+    displacement = [0, 0, cell[0, 0] / 2]
+    atoms.positions -= displacement
+    # atoms.positions += [0.1, 0.1, 0.1]
+    atoms.wrap()
+    atoms.center()
+    get_duplicate_atoms(atoms, cutoff=0.1, delete=True)
     
     # indice = []
     # for atom in atoms:
