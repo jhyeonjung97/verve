ad2ec943972493dcb02ebdd299d26d4b9c219f01
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-28 13:11:24 +0900

    .

--- tmp.sh
+++ tmp.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-# Get the current working directory
-current_dir=$(pwd)
-
-# Split the path into components
-IFS='/' read -r -a path_components <<< "$current_dir"
-
-# Extract the required parts and split by '_'
-metal=$(echo "${path_components[-3]}" | cut -d'_' -f2)
-spin=$(echo "${path_components[-2]}" | cut -d'_' -f2)
-dz=$(echo "${path_components[-1]}" | cut -d'_' -f1)
-
-# Print the results
-echo "Metal part: $metal"
-echo "Spin part: $spin"
-echo "DZ part: $dz"
+for dir in /pscratch/sd/j/jiuy97/6_MNC/0_clean/kisti/*d/*_*/*_*S/*_
+do
+    cd $dir; pwd
+    IFS='/' read -r -a path_components <<< $pwd
+    row=$(echo "${path_components[-4]}" | cut -d'_' -f1)
+    metal=$(echo "${path_components[-3]}" | cut -d'_' -f2)
+    spin=$(echo "${path_components[-2]}" | cut -d'_' -f2)
+    dz=$(echo "${path_components[-1]}" | cut -d'_' -f1)
+    
+    path4=${path_components[-4]}
+    path3=${path_components[-3]}
+    path2=${path_components[-2]}
+    path1=${path_components[-1]}
+    
+    if [[ ! -s DONE ]]; then
+        path=/pscratch/sd/j/jiuy97/6_MNC/0_clean/$path4/$path3/$path2/$path1
+        cp $path/WAVECAR .
+        cp $path/restart.json .
+        python ~/bin/tools/mnc/dz.py $dz
+        cp /pscratch/sd/j/jiuy97/6_MNC/0_clean/submit.sh .
+        sed -i -e "s/$jobname/$row$metal$spin$dz/" submit.sh
+        if [[ $spin == 'LS' ]]; then
+            sed -i -e "s/mnc-sol.py/mnc-sol-ls-nupdown.py/" submit.sh
+        elif [[ $spin == 'IS' ]]; then
+            sed -i -e "s/mnc-sol.py/mnc-sol-is-nupdown.py/" submit.sh
+        elif [[ $spin == 'HS' ]]; then
+            sed -i -e "s/mnc-sol.py/mnc-sol-hs-nupdown.py/" submit.sh
+        sbatch submit.sh
+    fi
+done
