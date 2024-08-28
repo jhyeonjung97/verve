085c2819e1c7f7ed573846eb52d419cff8be74ff
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-08-28 11:49:56 +0900

    .

--- resub.sh
+++ resub.sh
@@ -8,6 +8,7 @@ function usage_error {
 if [[ -z $1 ]]; then
     ~/bin/shoulder/rm_mv *.*.log
     ~/bin/shoulder/rm_mv *.e* *.o*
+    ~/bin/shoulder/rm_mv vasp.out  
     sh ~/bin/verve/sub.sh
     exit 0
 else
@@ -28,7 +29,7 @@ do
     cd $i*
     ~/bin/shoulder/rm_mv *.*.log
     ~/bin/shoulder/rm_mv *.e* *.o*
-    ~/bin/shoulder/rm_mv moments.*
+    ~/bin/shoulder/rm_mv vasp.out
     sh ~/bin/verve/sub.sh
     cd ..
 done
