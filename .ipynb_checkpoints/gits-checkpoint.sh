6f866ff60623b3325aafb39cd760d16dc84ef119
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-10-14 23:13:32 -0700

    .

--- .ipynb_checkpoints/gits-checkpoint.sh
+++ .ipynb_checkpoints/gits-checkpoint.sh
@@ -32,17 +32,17 @@ if [[ ${here} == 'nersc' ]] || [[ ${here} == 's3df' ]] || [[ ${here} == 'kisti' 
     do
         gits_nersc $i
         if [[ ${here} == 'nersc' ]] && [[ $i == 'tools' ]]; then
-            cp ~/bin/tools/mnc/* /pscratch/sd/j/jiuy97/6_MNC/scripts
-            cp ~/bin/tools/heo/* /pscratch/sd/j/jiuy97/5_HEO/scripts
-            cp ~/bin/tools/tetra/* /pscratch/sd/j/jiuy97/3_V_shape/scripts
+            cp -r ~/bin/tools/mnc/* /pscratch/sd/j/jiuy97/6_MNC/scripts
+            cp -r ~/bin/tools/heo/* /pscratch/sd/j/jiuy97/5_HEO/scripts
+            cp -r ~/bin/tools/tetra/* /pscratch/sd/j/jiuy97/3_V_bulk/scripts
         elif [[ ${here} == 'kisti' ]] && [[ $i == 'tools' ]]; then
-            cp ~/bin/tools/mnc/* /scratch/x2755a09/3_MNC/scripts
+            cp -r ~/bin/tools/mnc/* /scratch/x2755a09/3_MNC/scripts
             sed -i -e 's/kpar=8/kpar=1/' /scratch/x2755a09/3_MNC/scripts/*.py
             sed -i -e 's/npar=1/npar=8/' /scratch/x2755a09/3_MNC/scripts/*.py
-            cp ~/bin/tools/heo/* /scratch/x2755a09/4_HEO/scripts
+            cp -r ~/bin/tools/heo/* /scratch/x2755a09/4_HEO/scripts
             sed -i -e 's/kpar=8/kpar=8/' /scratch/x2755a09/4_HEO/scripts/*.py
             sed -i -e 's/npar=1/npar=8/' /scratch/x2755a09/4_HEO/scripts/*.py
-            cp ~/bin/tools/tetra/* /scratch/x2755a09/5_V_bulk/scripts
+            cp -r ~/bin/tools/tetra/* /scratch/x2755a09/5_V_bulk/scripts
             sed -i -e 's/kpar=8/kpar=4/' /scratch/x2755a09/5_V_bulk/scripts/opt*.py
             sed -i -e 's/npar=1/npar=16/' /scratch/x2755a09/5_V_bulk/scripts/opt*.py 
         fi
