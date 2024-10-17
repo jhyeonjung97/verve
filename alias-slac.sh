04fae3242e027f92a8f176e7ab6cce62b95b7aa7
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-10-17 13:17:14 -0700

    .

---
+++ alias-slac.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+alias cdw='cd /sdf/scratch/users/j/jiuy97/'
+alias cdp='cd /sdf/group/suncat/'
+
+alias vasp5='mv /global/homes/j/jiuy97/bin/vasp_outcar_parsers5.py /global/homes/j/jiuy97/.local/lib/python3.11/site-packages/ase/io/vasp_parsers/vasp_outcar_parsers.py'
+alias vasp6='mv /global/homes/j/jiuy97/bin/vasp_outcar_parsers6.py /global/homes/j/jiuy97/.local/lib/python3.11/site-packages/ase/io/vasp_parsers/vasp_outcar_parsers.py'
+alias mystat='squeue -o "%.10i %.9P %.16j %.8u %.8T %.8M %.10l %.6D %.15R" --me --sort=i'
+alias idle='squeue -o "%.10i %.9P %.16j %.8u %.8T %.8M %.10l %.6D %.15R" --me --sort=i | grep "30:00" | grep gpu
+squeue -o "%.10i %.9P %.16j %.8u %.8T %.8M %.10l %.6D %.15R" --me --sort=i | grep "30:00" | grep regular'
+
+alias qdel='scancel'
+
+alias k='python ~/bin/verve/show_kpoints.py POSCAR
+python ~/bin/verve/show_kpoints.py CONTCAR'
