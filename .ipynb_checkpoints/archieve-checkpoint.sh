fdd88b2fdc62c30631007a1fe7c160965b56737a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-19 11:39:39 -0800

    .

--- .ipynb_checkpoints/archieve-checkpoint.sh
+++ .ipynb_checkpoints/archieve-checkpoint.sh
@@ -1,7 +1,7 @@
 dir_now=$PWD
 
 if [[ ${here} == 'slac' ]]; then
-    cd /Users/jiuy97/OneDrive\ -\ Stanford
+    cd /Users/jiuy97/Library/CloudStorage/OneDrive-Stanford
 elif [[ ${here} == 'nersc' ]]; then
     sh ~/bin/touch-atime.sh
     exit
@@ -12,10 +12,11 @@ fi
 cp -r /Users/jiuy97/bin/orange/* ./bin/orange
 cp -r /Users/jiuy97/bin/shoulder/* ./bin/shoulder
 cp -r /Users/jiuy97/bin/verve/* ./bin/verve
+cp -r /Users/jiuy97/bin/tools/* ./bin/tools
 
 /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/1_cation .
 /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/2_icohp .
-/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/3_V_shape .
+/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/3_V_bulk .
 /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/4_V_slab .
 /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/5_HEO .
 /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/6_MNC .
--- archieve.sh
+++ archieve.sh
@@ -1,7 +1,7 @@
 dir_now=$PWD
 
 if [[ ${here} == 'slac' ]]; then
-    cd /Users/jiuy97/OneDrive\ -\ Stanford
+    cd /Users/jiuy97/Library/CloudStorage/OneDrive-Stanford
 elif [[ ${here} == 'nersc' ]]; then
     sh ~/bin/touch-atime.sh
     exit
@@ -12,6 +12,7 @@ fi
 cp -r /Users/jiuy97/bin/orange/* ./bin/orange
 cp -r /Users/jiuy97/bin/shoulder/* ./bin/shoulder
 cp -r /Users/jiuy97/bin/verve/* ./bin/verve
+cp -r /Users/jiuy97/bin/tools/* ./bin/tools
 
 /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/1_cation .
 /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/2_icohp .
