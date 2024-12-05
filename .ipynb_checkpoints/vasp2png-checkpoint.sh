5a04a61ea2991b00b394b5e31afc2b1b6ef8112a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-12-05 13:20:08 -0800

    .

--- .ipynb_checkpoints/vasp2png-checkpoint.sh
+++ .ipynb_checkpoints/vasp2png-checkpoint.sh
@@ -5,3 +5,11 @@ do
         python ~/bin/verve/vasp2png.py
     fi
 done
+
+for dir in /pscratch/sd/j/jiuy97/6_MNC/*_O*/*/*/*;
+do
+    cd $dir; pwd
+    if [[ -s CONTCAR ]] && [[ ! -f side-view.png ]]; then
+        python ~/bin/verve/vasp2png.py
+    fi
+done
