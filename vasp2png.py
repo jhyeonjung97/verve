5c893f837c876bb8f67c16961415714fa7ea1fde
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-12-05 12:47:22 -0800

    .

--- vasp2png.py
+++ vasp2png.py
@@ -1,4 +1,4 @@
 from ase.io import read, write
 
-atoms = read(CONTCAR)  # Read VASP file
+atoms = read('CONTCAR')  # Read VASP file
 write('side-view.png', atoms, rotation='-90x, -90y, 0z', show_unit_cell=0)  # Write PNG
