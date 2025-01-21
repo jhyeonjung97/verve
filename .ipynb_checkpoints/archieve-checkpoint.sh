b20d3223747e7713226bf073f1602abc2e1bc77b
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-01-20 16:55:05 -0800

    .

--- .ipynb_checkpoints/archieve-checkpoint.sh
+++ .ipynb_checkpoints/archieve-checkpoint.sh
@@ -11,8 +11,6 @@ cp -r /Users/jiuy97/bin/shoulder/* ./bin/shoulder
 cp -r /Users/jiuy97/bin/verve/* ./bin/verve
 cp -r /Users/jiuy97/bin/tools/* ./bin/tools
 
-/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/3_V_bulk .
-/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/4_V_slab .
 /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/6_MNC .
 
 cd /Users/jiuy97/Library/CloudStorage/OneDrive-Personal
@@ -20,5 +18,10 @@ cd /Users/jiuy97/Library/CloudStorage/OneDrive-Personal
 /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/1_cation .
 /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/2_icohp .
 /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/5_HEO .
-  
+
+cd /Users/jiuy97/Library/CloudStorage/OneDrive-Personal
+
+/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/3_V_bulk .
+/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/4_V_slab .
+
 cd $dir_now
