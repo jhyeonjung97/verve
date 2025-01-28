a4785118c49d9d89879693182c4142a4531fba20
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-01-28 15:09:14 -0800

    .

--- vasp2png.py
+++ vasp2png.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 from ase.io import read, write
 
@@ -9,9 +8,3 @@ for vasp_file in os.listdir('.'):  # List all files in the current directory
         filename = os.path.splitext(vasp_file)[0]  # Get the filename without extension
         png_file = f"{filename}.png"  # Define output PNG filename
         write(png_file, atoms, rotation='0x, 0y, 0z', show_unit_cell=0)  # Write PNG
-=======
-from ase.io import read, write
-
-atoms = read('CONTCAR')  # Read VASP file
-write('side-view.png', atoms, rotation='-90x, -90y, 0z', show_unit_cell=0)  # Write PNG
->>>>>>> 392149b737b6b94a0bcd9cb166c6d06a476b27d9
