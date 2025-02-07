17b8386103f7d676026779478483b9da8e658519
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-07 03:43:43 -0800

    .

--- .ipynb_checkpoints/organize-checkpoint.sh
+++ .ipynb_checkpoints/organize-checkpoint.sh
@@ -109,44 +109,52 @@ done
 
 cp /global/homes/j/jiuy97/bin/verve/template .
 cathub make-folders template
-cp /global/homes/j/jiuy97/bin/verve/template-metal .
-cathub make-folders template-metal
-find ${destination_base} -path "*/MISSING:*" -delete
 
 for dir in ${source_base}/0_clean/*d/*_*/most_stable/relaxed; do
     if [[ -d "$dir" ]]; then
         IFS='/' read -r -a path <<< "$dir"
         metal=$(echo "${path[-3]}" | cut -d'_' -f2)
-        
-        dest_dir="${destination_base}/${metal}N4C26/001/${site1}/H"
-
-        
-        dest_dir="${destination_base}/${metal}N4C26/001"
-        mkdir -p "$dest_dir"
-        cp "$dir/final_with_calculator.json" "$dest_dir/"
-        echo "Copied final_with_calculator.json to $dest_dir"
-    else
-        echo "Directory does not exist: $dir"
+        sed "s/METAL/$metal/g" template-metal > template
+        cathub make-folders template
     fi
 done
 
-# dual_path="/pscratch/sd/j/jiuy97/cathub/JungTuning2025/VASP-6.3.2/PBE+U+D3+VASPsol/FeC26N4_fcc/001"
-# dual_metals=("Co" "Fe" "Mo")
-# for metal in "${dual_metals[@]}"; do
-#     json_dir="${destination_base}/${metal}N4C26/001/site2"
-#     dest_dir="${destination_base}/${metal}N4C26.organized/VASP-6.3.2/PBE+U+D3+VASPsol/${metal}C26N4/001"
-#     mkdir -p "$dest_dir"
-#     for dual_dir in "${dual_path}"/*; do
-#         dual_dir_name=$(basename "$dual_dir")       
-#         pattern="${dual_dir_name##*__}"
-#         ads1=$(echo "$pattern" | cut -d'_' -f1 | cut -d'@' -f1 | sed 's/star//g')
-#         ads2=$(echo "$pattern" | cut -d'_' -f2 | cut -d'@' -f1 | sed 's/star//g')
-#         dir_name="${ads1}-${ads2}"
-#         if [[ -f "${json_dir}/${dir_name}/final_with_calculator.json" ]]; then
-#             mkdir -p "${dest_dir}/${dual_dir_name}"
-#             cp "${json_dir}/${dir_name}/final_with_calculator.json" "${dest_dir}/${dual_dir_name}"
-#         fi
-#     done
+find ${destination_base} -path "*/MISSING:*" -delete
+
+# for dir in ${source_base}/0_clean/*d/*_*/most_stable/relaxed; do
+#     if [[ -d "$dir" ]]; then
+#         IFS='/' read -r -a path <<< "$dir"
+#         metal=$(echo "${path[-3]}" | cut -d'_' -f2)
+        
+#         dest_dir="${destination_base}/${metal}N4C26/001/${site1}/H"
+
+        
+#         dest_dir="${destination_base}/${metal}N4C26/001"
+#         mkdir -p "$dest_dir"
+#         cp "$dir/final_with_calculator.json" "$dest_dir/"
+#         echo "Copied final_with_calculator.json to $dest_dir"
+#     else
+#         echo "Directory does not exist: $dir"
+#     fi
 # done
 
-tree *.organized
+# # dual_path="/pscratch/sd/j/jiuy97/cathub/JungTuning2025/VASP-6.3.2/PBE+U+D3+VASPsol/FeC26N4_fcc/001"
+# # dual_metals=("Co" "Fe" "Mo")
+# # for metal in "${dual_metals[@]}"; do
+# #     json_dir="${destination_base}/${metal}N4C26/001/site2"
+# #     dest_dir="${destination_base}/${metal}N4C26.organized/VASP-6.3.2/PBE+U+D3+VASPsol/${metal}C26N4/001"
+# #     mkdir -p "$dest_dir"
+# #     for dual_dir in "${dual_path}"/*; do
+# #         dual_dir_name=$(basename "$dual_dir")       
+# #         pattern="${dual_dir_name##*__}"
+# #         ads1=$(echo "$pattern" | cut -d'_' -f1 | cut -d'@' -f1 | sed 's/star//g')
+# #         ads2=$(echo "$pattern" | cut -d'_' -f2 | cut -d'@' -f1 | sed 's/star//g')
+# #         dir_name="${ads1}-${ads2}"
+# #         if [[ -f "${json_dir}/${dir_name}/final_with_calculator.json" ]]; then
+# #             mkdir -p "${dest_dir}/${dual_dir_name}"
+# #             cp "${json_dir}/${dir_name}/final_with_calculator.json" "${dest_dir}/${dual_dir_name}"
+# #         fi
+# #     done
+# # done
+
+# tree *.organized
