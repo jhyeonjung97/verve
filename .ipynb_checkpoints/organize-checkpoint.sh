44ab3b6594aef12abd06fa83636e3130d4e648fc
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-07 02:51:47 -0800

    .

--- .ipynb_checkpoints/organize-checkpoint.sh
+++ .ipynb_checkpoints/organize-checkpoint.sh
@@ -108,8 +108,7 @@ dual_metals=("Co" "Fe" "Mo")
 for metal in "${dual_metals[@]}"; do
     dest_dir="${destination_base}/${metal}N4C26/001/site2"
     mkdir -p "$dest_dir"
-    for sub_dir in "${dual_path}"/*: do
-        echo $sub_dir
+    for sub_dir in "${dual_path}"/*; do
         dir_name=$(basename "$sub_dir")       
         pattern="${dir_name##*__}"
         ads1=$(echo "$pattern" | cut -d'_' -f1 | sed 's/star//g')
