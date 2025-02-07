f3968580df12c4f3f6c5702fc23d103bf10cf716
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-07 03:10:04 -0800

    .

--- .ipynb_checkpoints/organize-checkpoint.sh
+++ .ipynb_checkpoints/organize-checkpoint.sh
@@ -115,14 +115,12 @@ for metal in "${dual_metals[@]}"; do
     dest_dir="${destination_base}/${metal}N4C26.organized/VASP-6.3.2/PBE+U+D3+VASPsol/${metal}C26N4/001"
     mkdir -p "$dest_dir"
     for dual_dir in "${dual_path}"/*; do
-        if [[ -d "$dual_dir" ]]; then
-            dual_dir_name=$(basename "$sub_dir")       
-            pattern="${dual_dir_name##*__}"
-            ads1=$(echo "$pattern" | cut -d'_' -f1 | cut -d'@' -f1 | sed 's/star//g')
-            ads2=$(echo "$pattern" | cut -d'_' -f2 | cut -d'@' -f1 | sed 's/star//g')
-            dir_name="${ads1}-${ads2}"
-            mkdir -p "${dest_dir}/${sub_dir_name}"
-            cp "${json_dir}/${dir_name}/final_with_calculator.json" "${dest_dir}/${dual_dir_name}"
-        fi
+        dual_dir_name=$(basename "$dual_dir")       
+        pattern="${dual_dir_name##*__}"
+        ads1=$(echo "$pattern" | cut -d'_' -f1 | cut -d'@' -f1 | sed 's/star//g')
+        ads2=$(echo "$pattern" | cut -d'_' -f2 | cut -d'@' -f1 | sed 's/star//g')
+        dir_name="${ads1}-${ads2}"
+        mkdir -p "${dest_dir}/${dual_dir_name}"
+        cp "${json_dir}/${dir_name}/final_with_calculator.json" "${dest_dir}/${dual_dir_name}"
     done
 done
