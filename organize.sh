bcd505d36864a310dcad946e1c5152030bc8956c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-07 00:51:00 -0800

    .

--- organize.sh
+++ organize.sh
@@ -7,7 +7,7 @@ destination_base="/pscratch/sd/j/jiuy97/cathub"
 for dir in ${source_base}/*d/*_*/most_stable/relaxed; do
     if [[ -d "$dir" ]]; then
         IFS='/' read -r -a path <<< "$dir"
-        metal=$(echo "${path[-3]}" | cut -d'_' -f3)
+        metal=$(echo "${path[-3]}" | cut -d'_' -f2)
         dest_dir="${destination_base}/${metal}N4C26/001"
         mkdir -p "$dest_dir"
         cp "$dir/final_with_calculator.json" "$dest_dir/"
