fe8559c93fd45e05cd347a116206909cf4aee261
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-12 23:15:42 -0800

    .

--- cathub.sh
+++ cathub.sh
@@ -34,11 +34,12 @@ for dir in ${mnc}/0_clean/*d/*_*/most_stable; do
     cp "${dir}/ooh/final_with_calculator.json" "${dest_dir}/OOH.json"
     echo "Copied ${dir}/ooh/final_with_calculator.json to ${dest_dir}"
     
-    for dz in '0.0' '0.2' '0.4' '0.6' '0.8' '1.0' '1.2'; do
+    for n in {0..6}; do
+        dz=$(echo "$n * 0.2" | bc)
         dest_dir="${cathub}/${metal}N4C26_${dz}/001-${site1}"
         mkdir -p "${dest_dir}"
-        cp "${dir}/final_with_calculator.json" "${dest_dir}/empty_slab.json"
-        echo "Copied ${dir}/final_with_calculator.json to ${dest_dir}"
+        cp "${dir}/${n}/final_with_calculator.json" "${dest_dir}/empty_slab.json"
+        echo "Copied ${dir}/${n}/final_with_calculator.json to ${dest_dir}"
     done
 done
 
