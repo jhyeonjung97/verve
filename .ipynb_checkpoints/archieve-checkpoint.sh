6046621c99969a92a55dff10e8d789d99c161834
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-21 16:20:13 -0800

    .

--- .ipynb_checkpoints/archieve-checkpoint.sh
+++ .ipynb_checkpoints/archieve-checkpoint.sh
@@ -1,27 +1,11 @@
-dir_now=$PWD
-
-if [[ ! ${here} == 'slac' ]]; then
-    exit
+if [[ ${here} == 'slac' ]]; then
+    cd /Users/jiuy97/Library/CloudStorage/OneDrive-Stanford
+    
+    /usr/bin/rsync -avzpl ~/bin/orange ./bin
+    /usr/bin/rsync -avzpl ~/bin/shoulder ./bin
+    /usr/bin/rsync -avzpl ~/bin/verve ./bin
+    /usr/bin/rsync -avzpl ~/bin/tools ./bin
+    
+    /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/5_HEO .
+    /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/6_MNC .
 fi
-
-cd /Users/jiuy97/Library/CloudStorage/OneDrive-Stanford
-
-cp -r /Users/jiuy97/bin/orange/* ./bin/orange
-cp -r /Users/jiuy97/bin/shoulder/* ./bin/shoulder
-cp -r /Users/jiuy97/bin/verve/* ./bin/verve
-cp -r /Users/jiuy97/bin/tools/* ./bin/tools
-
-/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp --exclude bin jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/6_MNC .
-
-cd /Users/jiuy97/Library/CloudStorage/OneDrive-Personal
-
-/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/1_cation .
-/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/2_icohp .
-/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/5_HEO .
-
-cd /Volumes/TOSHIBA
-
-/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/3_V_bulk .
-/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/4_V_slab .
-
-cd $dir_now
