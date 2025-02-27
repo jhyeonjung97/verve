084aa0437f5c22e37fcc5507a4adfbd3fdb29abf
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-27 12:48:38 -0800

    .

--- .ipynb_checkpoints/archieve-checkpoint.sh
+++ .ipynb_checkpoints/archieve-checkpoint.sh
@@ -1,15 +1,26 @@
+stanford=""
+personal=""
+google=""
+
 if [[ ${here} == 'slac' ]]; then
     stanford='/Users/jiuy97/Library/CloudStorage/OneDrive-Stanford'
+    personal='/Users/jiuy97/Library/CloudStorage/OneDrive-Personal'
+    google='/Users/jiuy97/Library/CloudStorage/GoogleDrive-jiuy97@stanford.edu/My\ Drive'
 elif [[ ${here} == 'mac' ]]; then
-    stanford='/Users/jiuy97/Library/CloudStorage/OneDrive-Stanford'
+    stanford='/Users/hailey/Library/CloudStorage/OneDrive-Stanford'
 fi
 
-cd $stanford
+if [[ -n $stanford ]]; then
+    cd "$stanford" || { echo "Failed to change directory to $stanford"; exit 1; }
+    
+    /usr/bin/rsync -avzpl ~/bin/orange ./bin
+    /usr/bin/rsync -avzpl ~/bin/shoulder ./bin
+    /usr/bin/rsync -avzpl ~/bin/verve ./bin
+    /usr/bin/rsync -avzpl ~/bin/tools ./bin
+    
+    /usr/bin/rsync -e ssh -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/5_HEO .
+    /usr/bin/rsync -e ssh -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/6_MNC .
 
-/usr/bin/rsync -avzpl ~/bin/orange ./bin
-/usr/bin/rsync -avzpl ~/bin/shoulder ./bin
-/usr/bin/rsync -avzpl ~/bin/verve ./bin
-/usr/bin/rsync -avzpl ~/bin/tools ./bin
-
-/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/5_HEO .
-/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/6_MNC .
+    if [[ -n $google ]]; then
+        /usr/bin/rsync -avzpl $google google
+fi
