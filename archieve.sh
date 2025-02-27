ce52efcff465930bd57f65489acc4166428a0288
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-02-27 12:36:50 -0800

    .

--- archieve.sh
+++ archieve.sh
@@ -1,11 +1,15 @@
 if [[ ${here} == 'slac' ]]; then
-    cd /Users/jiuy97/Library/CloudStorage/OneDrive-Stanford
-    
-    /usr/bin/rsync -avzpl ~/bin/orange ./bin
-    /usr/bin/rsync -avzpl ~/bin/shoulder ./bin
-    /usr/bin/rsync -avzpl ~/bin/verve ./bin
-    /usr/bin/rsync -avzpl ~/bin/tools ./bin
-    
-    /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/5_HEO .
-    /usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/6_MNC .
+    stanford='/Users/jiuy97/Library/CloudStorage/OneDrive-Stanford'
+elif [[ ${here} == 'mac' ]]; then
+    stanford='/Users/jiuy97/Library/CloudStorage/OneDrive-Stanford'
 fi
+
+cd $stanford
+
+/usr/bin/rsync -avzpl ~/bin/orange ./bin
+/usr/bin/rsync -avzpl ~/bin/shoulder ./bin
+/usr/bin/rsync -avzpl ~/bin/verve ./bin
+/usr/bin/rsync -avzpl ~/bin/tools ./bin
+
+/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/5_HEO .
+/usr/bin/rsync -e ssh --ignore-times --size-only -avlzp jiuy97@perlmutter.nersc.gov:/pscratch/sd/j/jiuy97/6_MNC .
