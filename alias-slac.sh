f14ce88a1a7ea2b4ae681324b00462594b848631
jhyeonjung97 <hyeonjung@postech.ac.kr>
2026-02-22 22:50:43 -0800

    .

--- alias-slac.sh
+++ alias-slac.sh
@@ -7,9 +7,9 @@ alias cdp='cd /sdf/data/suncat/suncatlustre/'
 
 alias vasp5='mv /global/homes/j/jiuy97/bin/vasp_outcar_parsers5.py /global/homes/j/jiuy97/.local/lib/python3.11/site-packages/ase/io/vasp_parsers/vasp_outcar_parsers.py'
 alias vasp6='mv /global/homes/j/jiuy97/bin/vasp_outcar_parsers6.py /global/homes/j/jiuy97/.local/lib/python3.11/site-packages/ase/io/vasp_parsers/vasp_outcar_parsers.py'
-alias mystat='squeue -o "%.10i %.9P %.16j %.8u %.8T %.8M %.10l %.6D %.15R" --me --sort=i'
+alias mystat='squeue -o "%.10i %.9P %.16j %.6u %.7T %.10M %.10l %.6D %.15R" --me --sort=i'
 alias idle='squeue -o "%.10i %.9P %.16j %.8u %.8T %.8M %.10l %.6D %.15R" --me --sort=i | grep "30:00" | grep gpu
-squeue -o "%.10i %.9P %.16j %.6u %.10T %.8M %.10l %.6D %.15R" --me --sort=i | grep "30:00" | grep regular'
+squeue -o "%.10i %.9P %.16j %.8u %.8T %.8M %.10l %.6D %.15R" --me --sort=i | grep "30:00" | grep regular'
 
 alias qdel='scancel'
 
