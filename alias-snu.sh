253667f66410841a4d70903399e5b8d16739dc1a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2026-02-26 20:31:28 -0800

    .

--- alias-snu.sh
+++ alias-snu.sh
@@ -18,3 +18,46 @@ python ~/bin/verve/show_kpoints.py CONTCAR'
 alias va='tail */vib/stdout.*.out'
 alias ta='tail */vasp.out'
 alias restart='sh ~/bin/verve/correct-contcar.sh; python ~/bin/get_restart.py'
+
+alias idle='pestat -s idle'
+alias mystat='qstat -u hyeonjung'
+alias mystat-g='
+echo "g1"
+qstat -u hyeonjung | grep --colour g1
+echo "g2"
+qstat -u hyeonjung | grep --colour g2
+echo "g3"
+qstat -u hyeonjung | grep --colour g3
+echo "g4"
+qstat -u hyeonjung | grep --colour g4'
+alias g1='qstat | grep -i "Q g1" '
+alias g2='qstat | grep -i "Q g2" '
+alias g3='qstat | grep -i "Q g3" '
+alias g4='qstat | grep -i "Q g4" '
+alias g='
+echo -e "\033[1mg1:\033[0m"
+g1
+echo -e "\033[1mg2:\033[0m"
+g2
+echo -e "\033[1mg3:\033[0m"
+g3
+echo -e "\033[1mg4:\033[0m"
+g4
+idle'
+
+alias p='echo "<g1>"
+squeue -o "%.10F %.10u %.20j %.2P %.5Q %.2t %.2Y" -S "t,-Q" | grep g1
+echo "<g2>"
+squeue -o "%.10F %.10u %.20j %.2P %.5Q %.2t %.2Y" -S "t,-Q" | grep g2
+echo "<g3>"
+squeue -o "%.10F %.10u %.20j %.2P %.5Q %.2t %.2Y" -S "t,-Q" | grep g3
+echo "<g4>"
+squeue -o "%.10F %.10u %.20j %.2P %.5Q %.2t %.2Y" -S "t,-Q" | grep g4'
+alias p1='squeue -o "%.10F %.10u %.20j %.2P %.5Q %.2t %.2Y" -S "t,-Q" | grep g1'
+alias p2='squeue -o "%.10F %.10u %.20j %.2P %.5Q %.2t %.2Y" -S "t,-Q" | grep g2'
+alias p3='squeue -o "%.10F %.10u %.20j %.2P %.5Q %.2t %.2Y" -S "t,-Q" | grep g3'
+alias p4='squeue -o "%.10F %.10u %.20j %.2P %.5Q %.2t %.2Y" -S "t,-Q" | grep g4'
+alias pestat1='pestat -N | grep g1'
+alias pestat2='pestat -N | grep g2'
+alias pestat3='pestat -N | grep g3'
+alias pestat4='pestat -N | grep g4'
